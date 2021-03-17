380a1b27449d71c943b6611f9c9e9c4b8e7a23a4
qicongsheng <875881559@qq.com>
2021-03-18 00:36:02 +0800

    Update convert2mp4.py

--- cloudreve/convert2mp4.py
+++ cloudreve/convert2mp4.py
@@ -51,6 +51,14 @@ def convert():
             ffmpeg(file_path, target_file_path)
             cursor.execute("update files set name ='%s',source_name='%s' where ID=%s" % (target_file_name, target_file_path, id))
             conn.commit()
+        if not file_name.endswith('.mp4') and file_name.endswith('.rm'):
+            target_file_name = file_name.replace('.rm', '.mp4')
+            target_file_path = file_path.replace('.rm', '.mp4')
+            cursor.execute("update files set name ='%s' where ID=%s" % ("[转码中...]" + file_name, id))
+            conn.commit()
+            ffmpeg(file_path, target_file_path)
+            cursor.execute("update files set name ='%s',source_name='%s' where ID=%s" % (target_file_name, target_file_path, id))
+            conn.commit()
         if not file_name.endswith('.mp4') and file_name.endswith('.wmv'):
             target_file_name = file_name.replace('.wmv', '.mp4')
             target_file_path = file_path.replace('.wmv', '.mp4')
