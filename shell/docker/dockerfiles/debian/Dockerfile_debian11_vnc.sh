e33222ead7f8e1671f88e91f75205f5f8c20cdb5
qicongsheng <875881559@qq.com>
2023-09-10 17:04:06 +0800

    Delete shell/docker/dockerfiles/debian/Dockerfile_debian11_vnc.sh

--- shell/docker/dockerfiles/debian/Dockerfile_debian11_vnc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt install wget curl unzip -y
-mkdir /tmp/work
-cd /tmp/work
-curl -o /tmp/work/Dockerfile https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian11_vnc
-docker build -t qics/debian:vnc .
-docker push qics/debian:vnc
-
