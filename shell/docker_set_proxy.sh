14092d8f30eec7a0f8672635e59ad9c1fcb5b2c0
qicongsheng <875881559@qq.com>
2023-09-11 12:05:37 +0800

    Update docker_set_proxy.sh

--- shell/docker_set_proxy.sh
+++ shell/docker_set_proxy.sh
@@ -4,7 +4,7 @@ mkdir -p /etc/systemd/system/docker.service.d
 
 cat >>/etc/systemd/system/docker.service.d/http-proxy.conf <<EOF
 [Service]
-Environment="HTTP_PROXY=http://play.qics.top:48118/" "HTTPS_PROXY=http://play.qics.top:8118/" "NO_PROXY=localhost,127.0.0.1,registry.docker-cn.com,hub-mirror.c.163.com"
+Environment="HTTP_PROXY=http://play.qics.top:8118/" "HTTPS_PROXY=http://play.qics.top:8118/" "NO_PROXY=localhost,127.0.0.1,registry.docker-cn.com,hub-mirror.c.163.com"
 EOF
 
 # 刷新配置项
