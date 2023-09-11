fa20938f89516cc8302631df339ed5ad0fcf0954
qicongsheng <875881559@qq.com>
2023-09-11 17:00:57 +0800

    Update tencent_init.sh

--- shell/vps/tencent_init.sh
+++ shell/vps/tencent_init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/vps/tencent_init.sh" | /bin/bash
+
+#setup
 apt-get update
 apt-get install ufw curl wget telnet dnsutils cron nload htop -y
 mkdir -p /var/spool/cron/crontabs
