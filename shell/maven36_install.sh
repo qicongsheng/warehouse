fce479ca3ddd79c61027a58e0e88af0b51cdb95d
qicongsheng <875881559@qq.com>
2023-09-12 17:15:11 +0800

    Update maven36_install.sh

--- shell/maven36_install.sh
+++ shell/maven36_install.sh
@@ -3,11 +3,7 @@
 rm -fr /tmp/apache-maven-3.6.3
 rm -fr /tmp/apache-maven-3.6.3.tar.gz
 curl -o /tmp/apache-maven-3.6.3.tar.gz -L https://rosw8w.sn.files.1drv.com/y4mMP0dpw9PeZ-Guymko-q4adinTBrq_7DIml47yT131r7lVwwDdL-RCz2rId6o-_oB3tJUlj4s717OiJWEv7oPVC7SvR219LX8UsXYVIf8ptCPBJt0wxI-xZZstotT-hrzyC13xkj9KtRnitfV4ftuQh4oqUenvfTZTH0tOZMhuxugRc9TT30O67yQ-YXUUhv5s955dlOR2Np0dukzBMxgdQ
-if [ ! -d "/usr/java" ];then
-  mkdir /usr/java
-  else
-  echo "/usr/java exist"
-fi
+mkdir -p /usr/java
 
 tar -zxvf /tmp/apache-maven-3.6.3.tar.gz -C /usr/java
 
