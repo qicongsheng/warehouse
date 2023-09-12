4bc4fa5b8e93aa54e245dbd3f9a2ea1c3464f298
qicongsheng <875881559@qq.com>
2023-09-12 17:14:57 +0800

    Update jdk8_install.sh

--- shell/jdk8_install.sh
+++ shell/jdk8_install.sh
@@ -3,11 +3,7 @@
 rm -fr /tmp/jdk1.8.0_241
 rm -fr /tmp/jdk-8u241-linux-x64.tar.gz
 curl -o /tmp/jdk-8u241-linux-x64.tar.gz -L https://rptm3a.sn.files.1drv.com/y4msYjjKamiuLERBxDSYgSLC2rVLtWlMAwncxS-93h5dCra-1hFufgzuy-hrxDRUzcSRB4S0Y-nmIEkFJmVBTi7SSWZtEU7VBr-zjcpaHi5_vkQkmHU4-u6Wa49OaBZl6x7weTh5jqF-mUkeXMD2w7W8k71B9pns9wWqQX75jcCtSM0BEaw8h5GE8bEW8mE0n94lWEIErCvpAXPftHHO8QQyA
-if [ ! -d "/usr/java" ];then
-  mkdir /usr/java
-  else
-  echo "/usr/java exist"
-fi
+mkdir -p /usr/java
 
 tar -zxvf /tmp/jdk-8u241-linux-x64.tar.gz -C /usr/java
 
