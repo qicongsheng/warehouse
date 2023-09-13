805c96e6acec00c8902b924b731c20b354de337c
qicongsheng <875881559@qq.com>
2023-09-13 17:32:00 +0800

    Update vultr_init.sh

--- shell/vps/vultr_init.sh
+++ shell/vps/vultr_init.sh
@@ -18,9 +18,6 @@ echo '' >> /etc/ssh/sshd_config
 echo 'Port 10088' >> /etc/ssh/sshd_config
 systemctl restart sshd.service
 
-#修改root密码
-echo root:@Best121^6789#@ | chpasswd
-
 #ls & ll
 echo "alias ls='ls \$LS_OPTIONS --color'" >> /root/.bashrc
 echo "alias ll='ls \$LS_OPTIONS -l --color'" >> /root/.bashrc
@@ -37,7 +34,6 @@ systemctl enable docker
 service docker restart
 docker run -d --restart=always -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
 docker run -d --restart=always -e BIND_PORT=7000 -e DASHBOARD_PORT=7777 -e DASHBOARD_USER=qics -e DASHBOARD_PWD=123456 -e TOKEN=badboy --network host qics/frp:server
-docker run -d --restart=always -e USER=root -e PASSWD=Star8ks# -p 8022:22 --privileged=true -v /tmp:/tmp qics/debian
 
 #屏蔽暴力破解
 echo "*/1 * * * * curl -fsSL 'http://www.qics.top/shell/blockip.sh' | /bin/bash" >> /var/spool/cron/crontabs/root
