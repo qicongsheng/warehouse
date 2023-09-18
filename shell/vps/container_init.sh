aaf450b14f0de2a905233a352e80f283efcf1722
qicongsheng <875881559@qq.com>
2023-09-18 18:51:50 +0800

    Update container_init.sh

--- shell/vps/container_init.sh
+++ shell/vps/container_init.sh
@@ -5,6 +5,7 @@
 apt-get update
 apt-get install curl wget telnet dnsutils cron nload htop nmap net-tools iputils-ping -y
 mkdir -p /var/spool/cron/crontabs
+echo "/etc/init.d/cron restart" >> /root/.bashrc
 
 #屏蔽暴力破解
 echo "*/1 * * * * curl -fsSL 'http://www.qics.top/shell/blockip.sh' | /bin/bash" >> /var/spool/cron/crontabs/root
