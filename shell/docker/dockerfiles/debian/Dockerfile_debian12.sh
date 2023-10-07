85ee0c21cedf28cf2f49469f7b3bc5e09d470266
qicongsheng <875881559@qq.com>
2023-10-07 15:47:41 +0800

    Update Dockerfile_debian12.sh

--- shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
+++ shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
@@ -3,11 +3,8 @@ apt install unzip wget curl -y
 mkdir /tmp/work 
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
-docker build -t qics/test:debian_12 .
-docker push qics/test:debian_12
-
-
-
+docker build -t qics/debian .
+docker push qics/debian
 
 
 
