0405bfcdccb639695abf8591113474148918dcc5
qicongsheng <875881559@qq.com>
2023-10-10 00:40:51 +0800

    Update python39_install_debian_green.sh

--- shell/debian/python39_install_debian_green.sh
+++ shell/debian/python39_install_debian_green.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 apt-get update 
 apt-get install bzip2 build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
-rm -fr /tmp/Python-3.9.12
-rm -fr /tmp/Python-3.9.12.tar.xz
-curl -o /tmp/Python-3.9.12.tar.xz https://potm3a.sn.files.1drv.com/y4mLOcQY2Mv5TwB_2iy9a16VezAJWbq9x-LbvkAjq2q35Xto2HjYaJJuAThr5mGhVqW0-OcJ4_neILzMH_3osqvN6iPSTb3UNQx1OTuV2A9v4wePbzhWITB0wQTwJT2Zm4e3JpkuomJVz0LrBoQATMbSZVagK5ihP6NjZsqBPtmkoJUeNt2cYvsIpcVmmbkCpKwvhjFuXV_Zgc35ENFZFmiOA
-cd /tmp && tar -xvJf Python-3.9.12.tar.xz
-mkdir /usr/local/python3
-cd /tmp/Python-3.9.12
-./configure --prefix=/usr/local/python3 --with-ssl --enable-optimizations
-make altinstall
+rm -fr /tmp/python3.9.12.tar.gz
+curl -o /tmp/python3.9.12.tar.gz https://potm3a.sn.files.1drv.com/y4mLOcQY2Mv5TwB_2iy9a16VezAJWbq9x-LbvkAjq2q35Xto2HjYaJJuAThr5mGhVqW0-OcJ4_neILzMH_3osqvN6iPSTb3UNQx1OTuV2A9v4wePbzhWITB0wQTwJT2Zm4e3JpkuomJVz0LrBoQATMbSZVagK5ihP6NjZsqBPtmkoJUeNt2cYvsIpcVmmbkCpKwvhjFuXV_Zgc35ENFZFmiOA
+tar -zxvf /tmp/python3.9.12.tar.gz -C /usr/local
 echo "export PATH=\$PATH:/etc/sbin:/usr/local/python3/bin" >> /etc/profile
 source /etc/profile
 ln -sf /usr/local/python3/bin/python3.9 /usr/local/bin/python3
 ln -sf /usr/local/python3/bin/python3.9 /bin/python3
 ln -sf /usr/local/python3/bin/pip3.9 /usr/local/bin/pip3
-rm -fr /tmp/Python-3.9.12
-rm -fr /tmp/Python-3.9.12.tar.xz
+rm -fr /tmp/python3.9.12.tar.gz
 
