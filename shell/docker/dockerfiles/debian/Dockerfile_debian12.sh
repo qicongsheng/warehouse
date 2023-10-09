34ae9c8969d741098eb9004378b446aad874fc8a
qicongsheng <875881559@qq.com>
2023-10-09 15:15:13 +0800

    Update Dockerfile_debian12.sh

--- shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
+++ shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt install unzip wget curl -y
+# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12.sh" | /bin/sh
 mkdir /tmp/work 
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
