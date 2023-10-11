1ea8107eea09d0a9e4620c0ed4a0ccc614e923ad
qicongsheng <875881559@qq.com>
2023-10-11 22:58:53 +0800

    Add files via upload

---
+++ shell/docker/dockerfiles/frp/docker_frp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -fr /tmp/work 
+mkdir /tmp/work 
+cd /tmp/work 
+curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/server/Dockerfile
+docker build -t qics/frp:server_0.52.1 .
+docker push qics/frp:server_0.52.1
+
+rm -fr  /tmp/work/Dockerfile
+curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/client/Dockerfile
+docker build -t qics/frp:client_0.52.1 .
+docker push qics/frp:client_0.52.1
+
+docker tag qics/frp:server_0.52.1 qics/frp:server
+docker tag qics/frp:client_0.52.1 qics/frp:client
+
+docker push qics/frp:server
+docker push qics/frp:client
