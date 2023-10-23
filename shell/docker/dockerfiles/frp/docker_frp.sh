c99f2159c9718ed2f1c14160002dba86b0546471
qicongsheng <875881559@qq.com>
2023-10-23 14:35:33 +0800

    Update docker_frp.sh

--- shell/docker/dockerfiles/frp/docker_frp.sh
+++ shell/docker/dockerfiles/frp/docker_frp.sh
@@ -17,3 +17,13 @@ docker tag qics/frp:client_0.52.2 qics/frp:client
 
 docker push qics/frp:server
 docker push qics/frp:client
+
+# -------------------------------------------------
+rm -fr /tmp/work 
+mkdir /tmp/work 
+cd /tmp/work 
+curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/client_pi/Dockerfile
+docker build -t qics/frp:client_pi_0.52.2 .
+docker push qics/frp:client_pi_0.52.2
+
+docker tag qics/frp:client_pi_0.52.2 qics/frp:client_pi
