202853ab108525d20618ef001fce2db5fe3aebeb
qicongsheng <875881559@qq.com>
2023-10-25 13:17:28 +0800

    Update docker_frp.sh

--- shell/docker/dockerfiles/frp/docker_frp.sh
+++ shell/docker/dockerfiles/frp/docker_frp.sh
@@ -4,16 +4,16 @@ rm -fr /tmp/work
 mkdir /tmp/work 
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/server/Dockerfile
-docker build -t qics/frp:server_0.52.2 .
-docker push qics/frp:server_0.52.2
+docker build -t qics/frp:server_0.52.3 .
+docker push qics/frp:server_0.52.3
 
 rm -fr  /tmp/work/Dockerfile
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/client/Dockerfile
-docker build -t qics/frp:client_0.52.2 .
-docker push qics/frp:client_0.52.2
+docker build -t qics/frp:client_0.52.3 .
+docker push qics/frp:client_0.52.3
 
-docker tag qics/frp:server_0.52.2 qics/frp:server
-docker tag qics/frp:client_0.52.2 qics/frp:client
+docker tag qics/frp:server_0.52.3 qics/frp:server
+docker tag qics/frp:client_0.52.3 qics/frp:client
 
 docker push qics/frp:server
 docker push qics/frp:client
@@ -23,10 +23,10 @@ rm -fr /tmp/work
 mkdir /tmp/work 
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/client_pi/Dockerfile
-docker build -t qics/frp:client_pi_0.52.2 .
-docker push qics/frp:client_pi_0.52.2
+docker build -t qics/frp:client_pi_0.52.3 .
+docker push qics/frp:client_pi_0.52.3
 
-docker tag qics/frp:client_pi_0.52.2 qics/frp:client_pi
+docker tag qics/frp:client_pi_0.52.3 qics/frp:client_pi
 docker push qics/frp:client_pi
 
 
