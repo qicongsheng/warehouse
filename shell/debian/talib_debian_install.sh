d0ff28b8c79e0926369d6af57686bd852869f3b0
qicongsheng <875881559@qq.com>
2023-11-20 17:39:54 +0800

    Update talib_debian_install.sh

--- shell/debian/talib_debian_install.sh
+++ shell/debian/talib_debian_install.sh
@@ -5,7 +5,6 @@
 apt-get install gcc-multilib build-essential -y
 rm -fr /tmp/ta-lib* /tmp/TA-Lib*
 curl -o /tmp/ta-lib-0.4.0-src.tar.gz https://www.qics.top/software/ta-lib/ta-lib-0.4.0-src.tar.gz
-curl -o /tmp/TA-Lib-0.4.26.tar.gz https://www.qics.top/software/ta-lib/TA-Lib-0.4.26.tar.gz
 cd /tmp && tar -zxvf /tmp/ta-lib-0.4.0-src.tar.gz
 cd /tmp/ta-lib && ./configure --prefix=/usr
 make && make install
@@ -13,7 +12,7 @@ export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> /etc/profile
 source /etc/profile
 pip3 install pip --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple
-cd /tmp && pip3 install TA-Lib-0.4.26.tar.gz
+cd /tmp && pip3 install TA-Lib
 rm -fr /tmp/ta-lib* /tmp/TA-Lib*
 
 
