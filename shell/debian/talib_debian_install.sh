47cffd6929dd1f98e12925ad7372cb9e52db081a
qicongsheng <875881559@qq.com>
2023-12-02 16:39:40 +0800

    Update talib_debian_install.sh

--- shell/debian/talib_debian_install.sh
+++ shell/debian/talib_debian_install.sh
@@ -3,18 +3,17 @@
 # curl -fsSL "https://www.qics.top/shell/debian/talib_debian_install.sh" | /bin/bash
 # =================================
 apt-get install gcc-multilib build-essential -y
-rm -fr /tmp/ta-lib* /tmp/TA-Lib*
+rm -fr /tmp/ta-lib*
 curl -o /tmp/ta-lib-0.4.0-src.tar.gz https://www.qics.top/software/ta-lib/ta-lib-0.4.0-src.tar.gz
-curl -o /tmp/TA-Lib-0.4.28.tar.gz https://www.qics.top/software/ta-lib/TA-Lib-0.4.28.tar.gz
 cd /tmp && tar -zxvf /tmp/ta-lib-0.4.0-src.tar.gz
 cd /tmp/ta-lib && ./configure --prefix=/usr
 make && make install
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> /etc/profile
 source /etc/profile
-pip3 install pip --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple
-cd /tmp && pip3 install TA-Lib-0.4.28.tar.gz
-rm -fr /tmp/ta-lib* /tmp/TA-Lib*
+pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple pip
+pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple ta-lib
+rm -fr /tmp/ta-lib*
 
 
 
