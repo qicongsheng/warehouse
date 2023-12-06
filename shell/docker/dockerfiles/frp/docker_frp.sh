6dba9db8ae72527d72a3f9ccf6741009c9ee6c18
qicongsheng <875881559@qq.com>
2023-12-06 15:34:11 +0800

    frp

--- shell/docker/dockerfiles/frp/docker_frp.sh
+++ shell/docker/dockerfiles/frp/docker_frp.sh
@@ -4,18 +4,16 @@ rm -fr /tmp/work
 mkdir /tmp/work 
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_server
-docker build -t qics/frp:server_0.52.3 .
+docker buildx build --build-arg FRP_VERSION=0.52.3 --platform linux/amd64,linux/arm64 -t qics/frp:server_0.52.3 . --push
 docker push qics/frp:server_0.52.3
+docker buildx build --build-arg FRP_VERSION=0.52.3 --platform linux/amd64,linux/arm64 -t qics/frp:server . --push
+docker push qics/frp:server
 
 rm -fr  /tmp/work/Dockerfile
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_client_amd64
-docker build -t qics/frp:client_0.52.3 .
+docker buildx build --build-arg FRP_VERSION=0.52.3 --platform linux/amd64,linux/arm64 -t qics/frp:client_0.52.3 . --push
 docker push qics/frp:client_0.52.3
-
-docker tag qics/frp:server_0.52.3 qics/frp:server
-docker tag qics/frp:client_0.52.3 qics/frp:client
-
-docker push qics/frp:server
+docker buildx build --build-arg FRP_VERSION=0.52.3 --platform linux/amd64,linux/arm64 -t qics/frp:client . --push
 docker push qics/frp:client
 
 
