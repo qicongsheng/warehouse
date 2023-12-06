374c2d025ff90151bb26507a0ab949b0300ea528
qicongsheng <875881559@qq.com>
2023-12-06 23:05:04 +0800

    ufw

--- shell/ufw/ufw_tencent.sh
+++ shell/ufw/ufw_tencent.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-echo `date '+%Y-%m-%d %H:%M:%S'` "ufw set start..."
+echo `date '+%Y-%m-%d %H:%M:%S'` "ufw set start..." >> /var/log/ufw.log
 echo y | /usr/sbin/ufw reset
 echo y | /usr/sbin/ufw enable
 /usr/sbin/ufw default allow
 
 home_ip=$(nslookup -d home.qics.top pranab.ns.cloudflare.com | awk '/^Address:/{ip=$2} END{print ip}')
-echo "home public ip $home_ip. add to ufw allow."
+echo "home public ip $home_ip. add to ufw allow." >> /var/log/ufw.log
+
 /usr/sbin/ufw allow from $home_ip
 
 /usr/sbin/ufw deny 22
@@ -60,4 +61,4 @@ echo "home public ip $home_ip. add to ufw allow."
 /usr/sbin/ufw deny 58022
 /usr/sbin/ufw deny 59022
 
-echo `date '+%Y-%m-%d %H:%M:%S'` "ufw set end"
+echo `date '+%Y-%m-%d %H:%M:%S'` "ufw set end" >> /var/log/ufw.log
