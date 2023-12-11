803ab5ea6138dc3cdb91629c878e9cc0b7a71039
qicongsheng <875881559@qq.com>
2023-12-11 12:10:54 +0800

    NGINX

--- shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
+++ shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
@@ -4,7 +4,7 @@ rm -fr /tmp/work
 mkdir /tmp/work 
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
-docker docker buildx build -t qics/debian --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t qics/debian --platform linux/amd64,linux/arm64 . --push
 
 
 
