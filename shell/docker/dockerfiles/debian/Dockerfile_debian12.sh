9d2328f70980f892539df17680afe78dc58b8843
qicongsheng <875881559@qq.com>
2023-12-11 12:08:26 +0800

    NGINX

--- shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
+++ shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12.sh" | /bin/sh
+rm -fr /tmp/work
 mkdir /tmp/work 
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
