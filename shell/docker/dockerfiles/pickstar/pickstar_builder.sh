4e563612cdb120319383514c4e50699e3aa28646
qicongsheng <875881559@qq.com>
2023-12-12 15:21:27 +0800

    pickstar

--- shell/docker/dockerfiles/pickstar/pickstar_builder.sh
+++ shell/docker/dockerfiles/pickstar/pickstar_builder.sh
@@ -5,7 +5,7 @@ mkdir /tmp/_pickstar
 cd /tmp/_pickstar
 curl -o /tmp/_pickstar/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/pickstar/Dockerfile
 docker build -t qics/pickstar .
-
+cd /tmp && rm -fr /tmp/_pickstar
 
 
 
