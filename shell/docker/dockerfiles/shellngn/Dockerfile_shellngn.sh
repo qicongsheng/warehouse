7bed6b59b0b830726ea2dd0330eb511cd95558e3
qicongsheng <875881559@qq.com>
2023-12-12 20:21:40 +0800

    pickstar

--- shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh
+++ shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh
@@ -4,7 +4,7 @@ rm -fr /tmp/_shellngn
 mkdir /tmp/_shellngn
 cd /tmp/_shellngn
 curl -o /tmp/_shellngn/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/shellngn/Dockerfile
-curl -o /tmp/_shellngn/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/shellngn/bundle.js
+curl -o /tmp/_shellngn/bundle.js -L https://www.qics.top/shell/docker/dockerfiles/shellngn/bundle.js
 docker build -t qics/shellngn:v1 .
 
 
