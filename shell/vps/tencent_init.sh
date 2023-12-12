32cc4969ac73b8ccfc20e4023d9414c9c72b1508
qicongsheng <875881559@qq.com>
2023-12-13 00:16:03 +0800

    pickstar

--- shell/vps/tencent_init.sh
+++ shell/vps/tencent_init.sh
@@ -30,6 +30,7 @@ service docker restart
 docker login -u=qics -p Best12167
 docker run -d --restart=always -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
 docker run -d --restart=always --net host --cap-add=NET_ADMIN -e NB_SETUP_KEY=951ABD0D-6D3D-47BB-AD78-A8F5D26F5DA7 qics/netbird
+docker run -d --restart=always -e API_KEY=LAdoeJAuaq_uB2kCblxZDtF5wJFrDYHQfSHOFge0 -e ZONE=qics.top -e SUBDOMAIN=play qics/ddns
 docker run -d --restart=always -e HOST=0.0.0.0 -e PORT=8066 --net host qics/shellngn
 docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123#  qics/mysql:5.7
 docker run -d --restart=always -e BIND_PORT=7000 -e DASHBOARD_PORT=7777 -e DASHBOARD_USER=qics -e DASHBOARD_PWD=123456 -e TOKEN=badboy --network host qics/frp:server
