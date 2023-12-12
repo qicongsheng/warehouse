ad752b35cc08fd2d3efdeb38ba619316c6de576d
qicongsheng <875881559@qq.com>
2023-12-12 15:24:37 +0800

    pickstar

--- shell/debian/talib_debian_install.sh
+++ shell/debian/talib_debian_install.sh
@@ -5,9 +5,6 @@
 apt-get install gcc-multilib build-essential -y
 rm -fr /tmp/ta-lib*
 curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/ta-lib-0.4.0-src.tar.gz https://www.qics.top/software/ta-lib/ta-lib-0.4.0-src.tar.gz
-echo "sleep 5s start"
-sleep 5
-echo "sleep 5s end"
 cd /tmp && tar -zxvf /tmp/ta-lib-0.4.0-src.tar.gz
 cd /tmp/ta-lib && ./configure --prefix=/usr
 make && make install
