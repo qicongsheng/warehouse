239c36746f5423c03ce189dca0638d8fe639a668
qicongsheng <875881559@qq.com>
2023-12-14 18:03:27 +0800

    pickstar

--- shell/docker/dockerfiles/deepin/Dockerfile_deepin.sh
+++ shell/docker/dockerfiles/deepin/Dockerfile_deepin.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/deepin/Dockerfile_deepin.sh" | /bin/sh
-mkdir /tmp/work 
+rm -fr /tmp/work
+mkdir /tmp/work
 cd /tmp/work 
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/deepin/Dockerfile
 docker build -t qics/deepin .
 docker push qics/deepin
-
+rm -fr /tmp/work
 
 
