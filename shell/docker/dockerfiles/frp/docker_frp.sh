d484833c9166ba28e755e9e288733e1d2ece34fd
qicongsheng <875881559@qq.com>
2023-12-15 23:54:10 +0800

    pickstar

--- shell/docker/dockerfiles/frp/docker_frp.sh
+++ shell/docker/dockerfiles/frp/docker_frp.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/frp/docker_frp.sh" | /bin/sh
-rm -fr /tmp/work 
-mkdir /tmp/work 
-cd /tmp/work 
-curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_server
+rm -fr /tmp/_frpserver
+mkdir /tmp/_frpserver
+cd /tmp/_frpserver
+curl -o /tmp/_frpserver/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_server
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:server_0.53.0 . --push
-docker push qics/frp:server_0.53.0
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:server . --push
-docker push qics/frp:server
 
-rm -fr  /tmp/work/Dockerfile
-curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_client_amd64
+
+rm -fr /tmp/_frpclient
+mkdir /tmp/_frpclient
+cd /tmp/_frpclient
+curl -o /tmp/_frpclient/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_client_amd64
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:client_0.53.0 . --push
-docker push qics/frp:client_0.53.0
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:client . --push
-docker push qics/frp:client
 
 
