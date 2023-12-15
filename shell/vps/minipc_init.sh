5bc2c10ed1a11f0a6d132754e947f4e6657b1f95
qicongsheng <875881559@qq.com>
2023-12-16 00:46:06 +0800

    pickstar

--- shell/vps/minipc_init.sh
+++ shell/vps/minipc_init.sh
@@ -1,8 +1,10 @@
-#cloudflared
+#!/bin/bash
+docker run -d --restart=always -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
 docker run -d --restart=always --net host --cap-add=NET_ADMIN -e NB_SETUP_KEY=951ABD0D-6D3D-47BB-AD78-A8F5D26F5DA7 qics/netbird
 docker run -d --restart=always --net host qics/cloudflared tunnel --no-autoupdate run --token eyJhIjoiZTAwMWM4MzlmMTNiNzU2ZDc0YWI1NjE0MzFlM2ZlZDciLCJ0IjoiMDM3N2MyNjYtMDg0OS00NTk0LWEzYTUtZTkyMThhYmRmNmQ0IiwicyI6IlltTm1OR1UyTURVdE5ESXpPUzAwTUdRNExXSTFOVEl0TVRCa05tRmpOemxqWW1FeCJ9
-docker run -d --restart=always -p 9000:80 -v /D/backup:/etc/nginx/html --privileged=true qics/nginx
-docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123#  qics/mysql:5.7
+docker run -d --restart=always --name proxy_mini_nginx -e SERVER_ADDR=play.qics.top -e PROXY_NAME=proxy_mini_nginx -e SERVER_PORT=7000 -e TOKEN=badboy -e LOCAL_PORT=9000 -e REMOTE_PORT=29000 --network host qics/frp:client
+docker run -d --restart=always --name proxy_mini_ssh_20088 -e SERVER_ADDR=play.qics.top -e PROXY_NAME=proxy_mini_ssh_20088 -e SERVER_PORT=7000 -e TOKEN=badboy -e LOCAL_PORT=10088 -e REMOTE_PORT=20088 --network host qics/frp:client
+docker run -d --restart=always --name proxy_mini_ssh_29022 -e SERVER_ADDR=play.qics.top -e PROXY_NAME=proxy_mini_ssh_29022 -e SERVER_PORT=7000 -e TOKEN=badboy -e LOCAL_PORT=9022 -e REMOTE_PORT=29022 --network host qics/frp:client
 docker run -d --restart=always -e USER=root -e PASSWD=Star8ks.# -p 127.0.0.1:9022:22 --privileged=true -v /tmp:/tmp qics/debian
 
 
