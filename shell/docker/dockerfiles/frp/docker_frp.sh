387037eeaf62a4c11159e09d38ff17d6683a578f
qicongsheng <875881559@qq.com>
2023-12-16 01:15:45 +0800

    pickstar

--- shell/docker/dockerfiles/frp/docker_frp.sh
+++ shell/docker/dockerfiles/frp/docker_frp.sh
@@ -6,6 +6,7 @@ cd /tmp/_frpserver
 curl -o /tmp/_frpserver/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_server
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:server_0.53.0 . --push
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:server . --push
+rm -fr /tmp/_frpserver
 
 
 rm -fr /tmp/_frpclient
@@ -14,5 +15,5 @@ cd /tmp/_frpclient
 curl -o /tmp/_frpclient/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/frp/Dockerfile_client
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:client_0.53.0 . --push
 docker buildx build --build-arg FRP_VERSION=0.53.0 --platform linux/amd64,linux/arm64 -t qics/frp:client . --push
-
+rm -fr /tmp/_frpclient
 
