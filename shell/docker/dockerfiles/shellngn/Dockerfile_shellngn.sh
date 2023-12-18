82c865c07108ae1144e1f2430103493721a08c24
qicongsheng <875881559@qq.com>
2023-12-18 16:53:05 +0800

    pickstar

--- shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh
+++ shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh
@@ -2,12 +2,11 @@
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh" | /bin/sh
 # docker cp pro-prod.sqlite 0705618ad574:/home/node/server/data/pro-prod.sqlite
 cd /tmp && rm -fr /tmp/_shellngn
-mkdir /tmp/_shellngn
-cd /tmp/_shellngn
+mkdir /tmp/_shellngn && cd /tmp/_shellngn
 curl -o /tmp/_shellngn/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/shellngn/Dockerfile
 curl -o /tmp/_shellngn/bundle.js -L https://www.qics.top/shell/docker/dockerfiles/shellngn/bundle.js
 docker build -t qics/shellngn:v1 .
-rm -fr /tmp/_shellngn
+cd /tmp && rm -fr /tmp/_shellngn
 
 
 
