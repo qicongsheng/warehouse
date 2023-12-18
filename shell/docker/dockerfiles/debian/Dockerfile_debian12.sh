1f72d486b7d527817b79b42fe6dcee3b752fabd7
qicongsheng <875881559@qq.com>
2023-12-18 16:46:17 +0800

    pickstar

--- shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
+++ shell/docker/dockerfiles/debian/Dockerfile_debian12.sh
@@ -2,11 +2,10 @@
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12.sh" | /bin/sh
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/pickstar/pickstar_builder.sh" | /bin/sh
 rm -fr /tmp/work
-mkdir /tmp/work 
-cd /tmp/work 
+mkdir /tmp/work && cd /tmp/work
 curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
 docker buildx build -t qics/debian --platform linux/amd64,linux/arm64 . --push
-
+cd /tmp && rm -fr /tmp/work
 
 
 
