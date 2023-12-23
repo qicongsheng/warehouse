d6e17789a368a5518e6542c20c8aaf5db98abd9c
qicongsheng <875881559@qq.com>
2023-12-23 23:29:22 +0800

    gitaction

---
+++ shell/docker/dockerfiles/debian/Dockerfile_vnc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_vnc.sh" | /bin/sh
+rm -fr /tmp/_vnc
+mkdir /tmp/_vnc && cd /tmp/_vnc
+curl -o /tmp/_vnc/debian-vnc.zip -L https://www.qics.top/shell/docker/dockerfiles/debian/vnc/debian-vnc.zip
+unzip debian-vnc.zip && cd debian-vnc.zip && chmod -R 777 *
+docker build -t qics/test:gui .
+
+cd /tmp && rm -fr /tmp/_vnc
+mkdir /tmp/_vnc && cd /tmp/_vnc
+curl -o /tmp/_vnc/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/vnc/Dockerfile
+docker build -t qics/debian:vnc . --push
+cd /tmp && rm -fr /tmp/_vnc
+
+
+
