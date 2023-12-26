55841c42d9d36382457b051feea3167e93eefc64
qicongsheng <875881559@qq.com>
2023-12-26 17:02:00 +0800

    gitaction

--- shell/docker/dockerfiles/dind/dind_build.sh
+++ shell/docker/dockerfiles/dind/dind_build.sh
@@ -2,7 +2,7 @@
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/dind_build.sh" | /bin/sh
 cd /tmp && rm -fr /tmp/_dind
 mkdir /tmp/_dind && cd /tmp/_dind
-curl -o /tmp/_dind/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/dind/Dockerfile
+curl -o /tmp/_dind/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/dind/Dockerfile
 docker buildx build -t qics/dind --platform linux/amd64,linux/arm64/v8 . --push
 cd /tmp && rm -fr /tmp/_dind
 
