a44a9954fc94b18c1228866e361f7ba689d72b3a
qicongsheng <875881559@qq.com>
2023-12-26 16:54:57 +0800

    gitaction

--- shell/docker/dockerfiles/dind/dind_build.sh
+++ shell/docker/dockerfiles/dind/dind_build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/Dockerfile" | /bin/sh
-rm -fr /tmp/_vnc
-mkdir /tmp/_vnc && cd /tmp/_vnc
-curl -o /tmp/_vnc/debian-vnc.zip -L https://www.qics.top/shell/docker/dockerfiles/debian/vnc/debian-vnc.zip
-unzip debian-vnc.zip && cd debian-vnc && chmod -R 777 *
-docker build -t qics/test:gui .
-
-cd /tmp && rm -fr /tmp/_vnc
-mkdir /tmp/_vnc && cd /tmp/_vnc
-curl -o /tmp/_vnc/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/vnc/Dockerfile
-docker build -t qics/debian:vnc . --push
-cd /tmp && rm -fr /tmp/_vnc
+# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/dind_build.sh" | /bin/sh
+cd /tmp && rm -fr /tmp/_dind
+mkdir /tmp/_dind && cd /tmp/_dind
+curl -o /tmp/_dind/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/dind/Dockerfile
+docker buildx build -t qics/dind --platform linux/amd64,linux/arm64/v8 . --push
+cd /tmp && rm -fr /tmp/_dind
 
 
 
