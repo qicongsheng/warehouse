829d3e4d6e27a0779fa3a9756315b3905924d354
qicongsheng <875881559@qq.com>
2023-12-27 15:12:46 +0800

    Update win10_init.sh

--- shell/vps/win10_init.sh
+++ shell/vps/win10_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run -d --restart=always -e API_KEY=LAdoeJAuaq_uB2kCblxZDtF5wJFrDYHQfSHOFge0 -e ZONE=qics.top -e SUBDOMAIN=home qics/ddns
+docker run -d --restart=always -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
 docker run -d --restart=always --net host --cap-add=NET_ADMIN -e NB_SETUP_KEY=951ABD0D-6D3D-47BB-AD78-A8F5D26F5DA7 qics/netbird
-docker run -d --restart=always --net host qics/cloudflared tunnel --no-autoupdate run --token eyJhIjoiZTAwMWM4MzlmMTNiNzU2ZDc0YWI1NjE0MzFlM2ZlZDciLCJ0IjoiMDM3N2MyNjYtMDg0OS00NTk0LWEzYTUtZTkyMThhYmRmNmQ0IiwicyI6IlltTm1OR1UyTURVdE5ESXpPUzAwTUdRNExXSTFOVEl0TVRCa05tRmpOemxqWW1FeCJ9
-docker run -d --restart=always --name proxy_mini_nginx -e SERVER_ADDR=play.qics.top -e PROXY_NAME=proxy_mini_nginx -e SERVER_PORT=7000 -e TOKEN=badboy -e LOCAL_PORT=9000 -e REMOTE_PORT=29000 --network host qics/frp:client
-docker run -d --restart=always -e USER=root -e PASSWD=Star8ks.# -p 127.0.0.1:9022:22 --privileged=true -v /tmp:/tmp qics/debian
-
+docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123# qics/mysql:5.7
+docker run -d --restart=always --net host qics/cloudflared tunnel --no-autoupdate run --token eyJhIjoiZTAwMWM4MzlmMTNiNzU2ZDc0YWI1NjE0MzFlM2ZlZDciLCJ0IjoiNWM4MzdiNWMtODljYS00YzVmLTlhOWMtZGJkZDU5ODQwNjdjIiwicyI6Ik1HUm1aalJpTldRdE5URmhZeTAwTTJFekxUaGtNV1V0WWpCaU5tTTNORE0zT0RZdyJ9
+docker run -d --restart=always -p 15672:15672 -p 5672:5672 qics/sc:rabbitmq
+docker run -d --restart=always -e REDIS_PASSWORD=Lhnekj#_83jf -p 6379:6379 qics/sc:redis /bin/sh -c 'redis-server --appendonly yes --requirepass ${REDIS_PASSWORD}'
 
