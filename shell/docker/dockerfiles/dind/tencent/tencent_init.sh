370acfb3c0ca6481f8a249879a7fead5e5761f4e
qicongsheng <875881559@qq.com>
2024-01-02 16:06:49 +0800

    gitaction

--- shell/docker/dockerfiles/dind/tencent/tencent_init.sh
+++ shell/docker/dockerfiles/dind/tencent/tencent_init.sh
@@ -3,7 +3,6 @@
 docker login -u=qics -p Best12167
 docker run -d --name nginx -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
 docker run -d --name netbird --net host --cap-add=NET_ADMIN -e NB_SETUP_KEY=951ABD0D-6D3D-47BB-AD78-A8F5D26F5DA7 qics/netbird
-docker run -d --name shellngn -e HOST=0.0.0.0 -e PORT=8066 --net host qics/shellngn
 docker run -d --name mysql_57 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123#  qics/mysql:5.7
 
 curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/pickstar.sql https://www.qics.top/software/mysql/database_create.sql
