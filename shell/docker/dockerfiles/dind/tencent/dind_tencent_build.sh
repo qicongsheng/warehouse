fc4d212a744670fb15dec4968047998b9ccffdea
qicongsheng <875881559@qq.com>
2024-01-02 14:34:58 +0800

    gitaction

--- shell/docker/dockerfiles/dind/tencent/dind_tencent_build.sh
+++ shell/docker/dockerfiles/dind/tencent/dind_tencent_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/vps/tencent_boot.sh" | /bin/sh
+# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/tencent/dind_tencent_build.sh" | /bin/sh
 docker run -d --name qics_dind --privileged=true -v /tmp:/tmp qics/dind
-docker exec qics_dind sh -c 'curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/vps/tencent_init.sh" | /bin/sh'
+docker exec qics_dind sh -c 'curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/tencent/tencent_init.sh" | /bin/sh'
 echo 'stoping qics_dind......'
 docker stop qics_dind
 echo 'stop qics_dind done......'
--- shell/docker/dockerfiles/dind/vps/tencent_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/dind_tencent_init.sh" | /bin/sh
-docker login -u=qics -p Best12167
-docker run -d --restart=always -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
-echo 'nginx start done......'
-docker run -d --restart=always --net host --cap-add=NET_ADMIN -e NB_SETUP_KEY=951ABD0D-6D3D-47BB-AD78-A8F5D26F5DA7 qics/netbird
-docker run -d --restart=always -e HOST=0.0.0.0 -e PORT=8066 --net host qics/shellngn
-docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123#  qics/mysql:5.7
-
