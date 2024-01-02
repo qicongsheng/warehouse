73de14e886c8cf0e2277f979cce858eedee5c4fe
qicongsheng <875881559@qq.com>
2024-01-02 15:03:25 +0800

    gitaction

--- shell/docker/dockerfiles/dind/tencent/dind_tencent_build.sh
+++ shell/docker/dockerfiles/dind/tencent/dind_tencent_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/tencent/dind_tencent_build.sh" | /bin/sh
 docker run -d --name qics_dind --privileged=true -v /tmp:/tmp qics/dind
+sleep 20
 docker exec qics_dind sh -c 'curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/dind/tencent/tencent_init.sh" | /bin/sh'
 echo 'stoping qics_dind......'
 docker stop qics_dind
