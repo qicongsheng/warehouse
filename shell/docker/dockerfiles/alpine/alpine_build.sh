2435e0158b4fa70635d07098be512c3334d3aa48
qicongsheng <875881559@qq.com>
2024-01-02 11:40:19 +0800

    gitaction

--- shell/docker/dockerfiles/alpine/alpine_build.sh
+++ shell/docker/dockerfiles/alpine/alpine_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/alpine/alpine_build.sh" | /bin/sh
-rm -fr /tmp/_alpine
+cd /tmp && rm -fr /tmp/_alpine
 mkdir /tmp/_alpine && cd /tmp/_alpine
 curl -o /tmp/_alpine/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/alpine/Dockerfile
 docker buildx build -t qics/alpine --platform linux/amd64,linux/arm64/v8 . --push
+cd /tmp && rm -fr /tmp/_alpine
