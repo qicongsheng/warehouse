f4748793f5d932abf4e8d7de34d2dffcf8830007
qicongsheng <875881559@qq.com>
2024-01-02 15:10:22 +0800

    gitaction

--- shell/docker/dockerfiles/dind/tencent/tencent_init.sh
+++ shell/docker/dockerfiles/dind/tencent/tencent_init.sh
@@ -3,3 +3,6 @@
 docker login -u=qics -p Best12167
 docker run -d --name nginx -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
 docker run -d --name netbird --net host --cap-add=NET_ADMIN -e NB_SETUP_KEY=951ABD0D-6D3D-47BB-AD78-A8F5D26F5DA7 qics/netbird
+
+docker stop $(docker ps -a -q)
+
