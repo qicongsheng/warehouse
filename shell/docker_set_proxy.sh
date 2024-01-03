799ea22458be6117b9487f54e9194e707df96221
qicongsheng <875881559@qq.com>
2024-01-03 14:00:48 +0800

    gitaction

--- shell/docker_set_proxy.sh
+++ shell/docker_set_proxy.sh
@@ -4,7 +4,7 @@ mkdir -p /etc/systemd/system/docker.service.d
 
 cat >>/etc/systemd/system/docker.service.d/http-proxy.conf <<EOF
 [Service]
-Environment="HTTP_PROXY=http://play.qics.top:8118/" "HTTPS_PROXY=http://play.qics.top:8118/" "NO_PROXY=localhost,127.0.0.1,registry.docker-cn.com,hub-mirror.c.163.com"
+Environment="HTTP_PROXY=http://hostvds.qics.top:6666/" "HTTPS_PROXY=http://play.qics.top:6666/" "NO_PROXY=localhost,127.0.0.1,registry.docker-cn.com,hub-mirror.c.163.com"
 EOF
 
 # 刷新配置项
