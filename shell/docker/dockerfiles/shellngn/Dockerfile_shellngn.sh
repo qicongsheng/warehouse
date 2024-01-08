16a8b2791f995fe4703f4791e079fe5adff61f8e
qicongsheng <875881559@qq.com>
2024-01-08 15:50:16 +0800

    gitaction

--- shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh
+++ shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/shellngn/Dockerfile_shellngn.sh" | /bin/sh
-# docker cp pro-prod.sqlite 0705618ad574:/home/node/server/data/pro-prod.sqlite
 cd /tmp && rm -fr /tmp/_shellngn
 mkdir /tmp/_shellngn && cd /tmp/_shellngn
 curl -o /tmp/_shellngn/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/shellngn/Dockerfile
