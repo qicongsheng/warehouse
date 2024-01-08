84c8f073f7b9bb88faf48e474e382bd206271e41
qicongsheng <875881559@qq.com>
2024-01-08 17:06:08 +0800

    gitaction

--- shell/docker/dockerfiles/shellngn/shellngn_builder.sh
+++ shell/docker/dockerfiles/shellngn/shellngn_builder.sh
@@ -3,6 +3,7 @@
 cd /tmp && rm -fr /tmp/_shellngn
 mkdir /tmp/_shellngn && cd /tmp/_shellngn
 docker login -u=qics -p Best12167
+curl -o /tmp/_shellngn/bundle.js -L https://www.qics.top/shell/docker/dockerfiles/shellngn/bundle.js
 curl -o /tmp/_shellngn/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/shellngn/Dockerfile
 docker build -t qics/shellngn . --push
 cd /tmp && rm -fr /tmp/_shellngn
