4ae5a6ec5ec2c5d2b398deb67e4519cc404a3251
qicongsheng <875881559@qq.com>
2024-01-08 16:55:53 +0800

    gitaction

--- shell/docker/dockerfiles/debian/debian12_builder.sh
+++ shell/docker/dockerfiles/debian/debian12_builder.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/debian/debian12_builder.sh" | /bin/sh
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/pickstar/pickstar_builder.sh" | /bin/sh
-rm -fr /tmp/work
-mkdir /tmp/work && cd /tmp/work
-curl -o /tmp/work/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
+rm -fr /tmp/_debian12
+mkdir /tmp/_debian12 && cd /tmp/_debian12
+curl -o /tmp/_debian12/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
 docker buildx build -t qics/debian --platform linux/amd64,linux/arm64 . --push
-cd /tmp && rm -fr /tmp/work
+cd /tmp && rm -fr /tmp/_debian12
 
 
 
