4c32c79e2e7f6ef899ab400036946ca3ce2a33f0
qicongsheng <875881559@qq.com>
2024-01-18 16:21:21 +0800

    gitaction

--- shell/ufw/ufw_vultr.sh
+++ shell/ufw/ufw_vultr.sh
@@ -12,72 +12,18 @@ echo "tencent ip $tencent_ip. add to ufw allow." >> /var/log/ufw.log
 /usr/sbin/ufw allow from $home_ip
 /usr/sbin/ufw allow from $tencent_ip
 
-/usr/sbin/ufw deny 19986
-/usr/sbin/ufw deny 29986
-/usr/sbin/ufw deny 39986
-/usr/sbin/ufw deny 49986
-/usr/sbin/ufw deny 59986
-
-/usr/sbin/ufw deny 22
-/usr/sbin/ufw deny 1022
-/usr/sbin/ufw deny 2022
-/usr/sbin/ufw deny 3022
-/usr/sbin/ufw deny 4022
-/usr/sbin/ufw deny 5022
-/usr/sbin/ufw deny 6022
-/usr/sbin/ufw deny 7022
-/usr/sbin/ufw deny 8022
-/usr/sbin/ufw deny 9022
-/usr/sbin/ufw deny 10088
-/usr/sbin/ufw deny 11022
-/usr/sbin/ufw deny 12022
-/usr/sbin/ufw deny 13022
-/usr/sbin/ufw deny 14022
-/usr/sbin/ufw deny 15022
-/usr/sbin/ufw deny 16022
-/usr/sbin/ufw deny 17022
-/usr/sbin/ufw deny 18022
-/usr/sbin/ufw deny 19022
-/usr/sbin/ufw deny 20088
-/usr/sbin/ufw deny 21022
-/usr/sbin/ufw deny 22022
-/usr/sbin/ufw deny 23022
-/usr/sbin/ufw deny 24022
-/usr/sbin/ufw deny 25022
-/usr/sbin/ufw deny 26022
-/usr/sbin/ufw deny 27022
-/usr/sbin/ufw deny 28022
-/usr/sbin/ufw deny 29022
-/usr/sbin/ufw deny 30088
-/usr/sbin/ufw deny 31022
-/usr/sbin/ufw deny 32022
-/usr/sbin/ufw deny 33022
-/usr/sbin/ufw deny 34022
-/usr/sbin/ufw deny 35022
-/usr/sbin/ufw deny 36022
-/usr/sbin/ufw deny 37022
-/usr/sbin/ufw deny 38022
-/usr/sbin/ufw deny 39022
-/usr/sbin/ufw deny 40088
-/usr/sbin/ufw deny 41022
-/usr/sbin/ufw deny 42022
-/usr/sbin/ufw deny 43022
-/usr/sbin/ufw deny 44022
-/usr/sbin/ufw deny 45022
-/usr/sbin/ufw deny 46022
-/usr/sbin/ufw deny 47022
-/usr/sbin/ufw deny 48022
-/usr/sbin/ufw deny 49022
-/usr/sbin/ufw deny 50088
-/usr/sbin/ufw deny 51022
-/usr/sbin/ufw deny 52022
-/usr/sbin/ufw deny 53022
-/usr/sbin/ufw deny 54022
-/usr/sbin/ufw deny 55022
-/usr/sbin/ufw deny 56022
-/usr/sbin/ufw deny 57022
-/usr/sbin/ufw deny 58022
-/usr/sbin/ufw deny 59022
+deny_ports=(
+19986 29986 39986 49986 59986
+22 1022 2022 3022 4022 5022 6022 7022 8022 9022
+10088 11022 12022 13022 14022 15022 16022 17022 18022 19022
+20088 21022 22022 23022 24022 25022 26022 27022 28022 29022
+30088 31022 32022 33022 34022 35022 36022 37022 38022 39022
+40088 41022 42022 43022 44022 45022 46022 47022 48022 49022
+50088 51022 52022 53022 54022 55022 56022 57022 58022 59022
+)
+for deny_port in "${deny_ports[@]}"; do
+    /usr/sbin/ufw deny $deny_port
+done
 
 sed -i '/ufw-before-input.*icmp/s/ACCEPT/DROP/g' /etc/ufw/before.rules
 /usr/sbin/ufw reload
