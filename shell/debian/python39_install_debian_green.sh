be037adc446aabd9d381a99bded74cd828b131d1
qicongsheng <875881559@qq.com>
2024-03-01 14:03:41 +0800

    gitaction

--- shell/debian/python39_install_debian_green.sh
+++ shell/debian/python39_install_debian_green.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get install bzip2 build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
 rm -fr /tmp/python-3.9.12-debian.tar.gz
 echo 'start download python-3.9.12-debian.tar.gz ...'
-curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/python-3.9.12-debian.tar.gz https://ehckxq.sn.files.1drv.com/y4msFMU-V_ZIaEEvZiVvir06mZSmkmvvhHs-HkhmS5H21UgJsROtFyn32ycnmgE8iEqiv8uSqsyNyyT-KeKlcc7m7zcwy6TcuUj-U3_0HLWHnbuB-2PNeHUR6G3BpCssi9DhqrFrhP-Mr3ia5P6CeTeGsWj7ZLPimHcd3A59EOfuazyM5TadnnHocAeQSL38Oe90K1ug7JxftKsUULfVuMBjg
+curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/python-3.9.12-debian.tar.gz https://fha48w.sn.files.1drv.com/y4mv_dgoOkGNYbbFaokTgrdv5Cifs0xGeahNLCec5oC9YByv7wbV1a4-CkM_zmow4OrPDpD8-aX9Wo-fmuf-PmW2SOjg3fhAfLPpMAsiJDv28ZEvyQgV-YU_F4ue_o80nL8_Om_dkEEosy7KPrz5OKwsSHpeWOGsR-3WaIdNbIaQ0sn_IhLiZqLUewOVOeLYvomKJqMtJ6ASzWzzvoPpmWiEA
 echo 'download python-3.9.12-debian.tar.gz end ...'
 tar -zxvf /tmp/python-3.9.12-debian.tar.gz -C /usr/local
 echo "export PATH=\$PATH:/etc/sbin:/usr/local/python3/bin" >> /etc/profile
