ce48ab6915088a045d5f009153e80650c6b3f127
qicongsheng <875881559@qq.com>
2024-03-04 18:08:56 +0800

    gitaction

---
+++ shell/docker/dockerfiles/playdocker/playdocker_builder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/playdocker/playdocker_builder.sh" | /bin/sh
+rm -fr /tmp/_playdocker
+mkdir /tmp/_playdocker
+cd /tmp/_playdocker
+curl -o /tmp/_playdocker/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/playdocker/Dockerfile
+docker build -t qics/chrome . --push
+cd /tmp && rm -fr /tmp/_playdocker
+
+
+
+
