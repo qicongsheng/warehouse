02b823ac9baf4e4920f3f4d20ba4cbcc9ac17bf8
qicongsheng <875881559@qq.com>
2024-03-04 19:13:52 +0800

    gitaction

--- shell/docker/dockerfiles/playdocker/playdocker_builder.sh
+++ shell/docker/dockerfiles/playdocker/playdocker_builder.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/playdocker/playdocker_builder.sh" | /bin/sh
+docker buildx create --name mutibuilder
+docker buildx use mutibuilder
+docker buildx inspect --bootstrap
 rm -fr /tmp/_playdocker
 mkdir /tmp/_playdocker
 cd /tmp/_playdocker
 curl -o /tmp/_playdocker/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/playdocker/Dockerfile
-docker build -t qics/playdocker . --push
+docker buildx build -t qics/playdocker --platform linux/amd64,linux/arm64/v8 . --push
 cd /tmp && rm -fr /tmp/_playdocker
 
 
