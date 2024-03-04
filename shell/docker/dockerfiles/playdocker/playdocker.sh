0c3122aaa64104c0a5f59db72e71d85a703046fc
qicongsheng <875881559@qq.com>
2024-03-04 19:32:25 +0800

    gitaction

--- shell/docker/dockerfiles/playdocker/playdocker.sh
+++ shell/docker/dockerfiles/playdocker/playdocker.sh
@@ -3,5 +3,6 @@ pip3 install pip --upgrade
 pip3 install paramiko loguru selenium
 echo 124565
 curl -fsSL "https://www.qics.top/shell/docker/dockerfile/playdocker/frp_vpn.py" | python3
+tail -f /dev/null
 
 
