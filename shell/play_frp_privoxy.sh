cccb5624a806f16e8a2577831891d7438932557f
qicongsheng <875881559@qq.com>
2024-03-06 00:10:38 +0800

    gitaction

--- shell/play_frp_privoxy.sh
+++ shell/play_frp_privoxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker run -d --name ssr_server -e PASSWORD=mimabujiandan -e METHOD=rc4-md5 -p 19986:8388 -p 19986:8388/udp qics/shadowsocks:server
 docker run -d --name ssr_client -e PASSWORD=mimabujiandan -e SERVER_ADDR=127.0.0.1 -e SERVER_PORT=19986 -e METHOD=rc4-md5 -p 1080:1080 -p 1080:1080/udp --net=host -d qics/shadowsocks:client
-docker run -d --name privoxy_server -e FORWARD_IP=127.0.0.1 -e FORWARD_PORT=1080 --cap-add NET_ADMIN --net=host -p 8118:8118 -p 8118:8118/udp qics/privoxy:latest
+docker run -d --name privoxy_server -e FORWARD_IP=127.0.0.1 -e FORWARD_PORT=1080 --cap-add=NET_ADMIN --net=host -p 8118:8118 -p 8118:8118/udp qics/privoxy:latest
 docker run -d --name privoxy_frp -e SERVER_ADDR=play.qics.top -e PROXY_NAME=proxy_play_privoxy -e SERVER_PORT=7000 -e TOKEN=badboy -e LOCAL_PORT=8118 -e REMOTE_PORT=48118 --net=host qics/frp:client
 
 
--- shell/play_oray_ssr.sh
+++ shell/play_oray_ssr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d --net=host --cap-add NET_ADMIN --env PGY_USERNAME="qicongsheng" --env PGY_PASSWORD="Best12167" qics/pgyvpn
+docker run -d --net=host --cap-add=NET_ADMIN --env PGY_USERNAME="qicongsheng" --env PGY_PASSWORD="Best12167" qics/pgyvpn
 docker run -d --restart=always --name ssr_server -e PASSWORD=mimabujiandan -e METHOD=aes-128-gcm -p 19986:8388 -p 19986:8388/udp qics/shadowsocks:server
 docker run -d -p 9000:80 -v /tmp:/etc/nginx/html --privileged=true qics/nginx
 
