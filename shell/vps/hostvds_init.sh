cafed9fee2f589ada27de6cfa539a1563d54eb29
qicongsheng <875881559@qq.com>
2024-03-08 23:17:40 +0800

    gitaction

--- shell/vps/hostvds_init.sh
+++ shell/vps/hostvds_init.sh
@@ -54,6 +54,6 @@ docker run -d --restart=always -p 9000:80 --privileged=true -v /tmp:/etc/nginx/h
 docker run -d --restart=always -e NB_SETUP_KEY=951ABD0D-6D3D-47BB-AD78-A8F5D26F5DA7 --net=host --privileged=true qics/netbird
 docker run -d --restart=always -e API_KEY=LAdoeJAuaq_uB2kCblxZDtF5wJFrDYHQfSHOFge0 -e ZONE=qics.top -e SUBDOMAIN=hostvds qics/ddns
 docker run -d --restart=always qics/traffmonetizer start accept --token 5xijhElDM8IqEwMj0VolDEVsbUDCGS3GFpRCFckxix8=
-docker run -d --restart=always --name proxy_hostvds_ssh_59022 -e SERVER_ADDR=play.qics.top -e PROXY_NAME=proxy_hostvds_ssh_59022 -e SERVER_PORT=7000 -e TOKEN=badboy -e LOCAL_PORT=9022 -e REMOTE_PORT=59022 --net=host qics/frp:client
+docker run -d --restart=always --name hostvds_ssh_59022 -e SERVER_ADDR=play.qics.top -e PROXY_NAME=hostvds_ssh_59022 -e SERVER_PORT=7000 -e TOKEN=badboy -e LOCAL_PORT=9022 -e REMOTE_PORT=59022 --net=host qics/frp:client
 docker run -d --restart=always -e USER=root -e PASSWD=Star8ks.# -p 127.0.0.1:9022:22 --privileged=true -v /tmp:/tmp qics/debian
 
