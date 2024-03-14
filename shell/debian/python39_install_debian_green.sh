aff29797947ec620f0ea127f2fd0088abb0f30b6
qicongsheng <875881559@qq.com>
2024-03-14 19:34:18 +0800

    gitaction

--- shell/debian/python39_install_debian_green.sh
+++ shell/debian/python39_install_debian_green.sh
@@ -3,17 +3,17 @@ apt-get update
 apt-get install bzip2 build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
 
 while true; do
-    rm -fr /tmp/python-3.9.12-debian.tar.gz
-    echo 'start download python-3.9.12-debian.tar.gz ...'
-    curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/python-3.9.12-debian.tar.gz https://fha48w.sn.files.1drv.com/y4mv_dgoOkGNYbbFaokTgrdv5Cifs0xGeahNLCec5oC9YByv7wbV1a4-CkM_zmow4OrPDpD8-aX9Wo-fmuf-PmW2SOjg3fhAfLPpMAsiJDv28ZEvyQgV-YU_F4ue_o80nL8_Om_dkEEosy7KPrz5OKwsSHpeWOGsR-3WaIdNbIaQ0sn_IhLiZqLUewOVOeLYvomKJqMtJ6ASzWzzvoPpmWiEA
-    echo 'download python-3.9.12-debian.tar.gz end ...'
-    tar -zxvf /tmp/python-3.9.12-debian.tar.gz -C /usr/local
-    if [ $? -eq 0 ]; then
-        echo "解压成功，退出循环"
-        break
-    else
-        echo "解压失败，重试"
-    fi
+  rm -fr /tmp/python-3.9.12-debian.tar.gz
+  echo 'start download python-3.9.12-debian.tar.gz ...'
+  curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/python-3.9.12-debian.tar.gz https://fha48w.sn.files.1drv.com/y4mv_dgoOkGNYbbFaokTgrdv5Cifs0xGeahNLCec5oC9YByv7wbV1a4-CkM_zmow4OrPDpD8-aX9Wo-fmuf-PmW2SOjg3fhAfLPpMAsiJDv28ZEvyQgV-YU_F4ue_o80nL8_Om_dkEEosy7KPrz5OKwsSHpeWOGsR-3WaIdNbIaQ0sn_IhLiZqLUewOVOeLYvomKJqMtJ6ASzWzzvoPpmWiEA
+  echo 'download python-3.9.12-debian.tar.gz end ...'
+  tar -zxvf /tmp/python-3.9.12-debian.tar.gz -C /usr/local
+  if [ $? -eq 0 ]; then
+    echo "解压成功，退出循环"
+      break
+  else
+    echo "解压失败，重试"
+  fi
 done
 
 echo "export PATH=\$PATH:/etc/sbin:/usr/local/python3/bin" >> /etc/profile
