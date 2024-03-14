af40002d100413aaeed41dd72e619ef2b9a6e262
qicongsheng <875881559@qq.com>
2024-03-14 19:33:40 +0800

    gitaction

--- shell/debian/talib_debian_install.sh
+++ shell/debian/talib_debian_install.sh
@@ -3,9 +3,21 @@
 # curl -fsSL "https://www.qics.top/shell/debian/talib_debian_install.sh" | /bin/bash
 # =================================
 apt-get install gcc-multilib build-essential -y
-rm -fr /tmp/ta-lib*
-curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/ta-lib-0.4.0-src.tar.gz https://www.qics.top/software/ta-lib/ta-lib-0.4.0-src.tar.gz
-cd /tmp && tar -zxvf /tmp/ta-lib-0.4.0-src.tar.gz
+
+while true; do
+  rm -fr /tmp/ta-lib*
+  echo 'start download ta-lib-0.4.0-src.tar.gz ...'
+  curl --connect-timeout 600 -m 7200 --continue-at - -o /tmp/ta-lib-0.4.0-src.tar.gz https://www.qics.top/software/ta-lib/ta-lib-0.4.0-src.tar.gz
+  echo 'download ta-lib-0.4.0-src.tar.gz end ...'
+  cd /tmp && tar -zxvf /tmp/ta-lib-0.4.0-src.tar.gz
+  if [ $? -eq 0 ]; then
+    echo "解压成功，退出循环"
+    break
+  else
+    echo "解压失败，重试"
+  fi
+done
+
 cd /tmp/ta-lib && ./configure --prefix=/usr
 make && make install
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
