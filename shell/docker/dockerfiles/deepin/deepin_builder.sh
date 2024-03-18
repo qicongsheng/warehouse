0e9e3d19304ecaa361b42373824ccdd28ee3dd1c
qicongsheng <875881559@qq.com>
2024-03-18 19:21:51 +0800

    gitaction

--- shell/docker/dockerfiles/deepin/deepin_builder.sh
+++ shell/docker/dockerfiles/deepin/deepin_builder.sh
@@ -3,8 +3,11 @@
 rm -fr /tmp/_deepin
 mkdir /tmp/_deepin
 cd /tmp/_deepin
+curl -o /tmp/_deepin/env -L https://www.qics.top/shell/docker/dockerfiles/deepin/env
+curl -o /tmp/_deepin/xstartup -L https://www.qics.top/shell/docker/dockerfiles/deepin/xstartup
 curl -o /tmp/_deepin/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/deepin/Dockerfile
 docker build -t qics/deepin . --push
 rm -fr /tmp/_deepin
+
 
 
