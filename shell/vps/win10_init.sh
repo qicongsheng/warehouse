4b299fe82e915003befeb01abbabdefd8ba7c6e6
qicongsheng <875881559@qq.com>
2024-03-19 09:04:27 +0800

    gitaction

--- shell/vps/win10_init.sh
+++ shell/vps/win10_init.sh
@@ -5,5 +5,5 @@ docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy1
 docker run -d --restart=always --net=host qics/cloudflared tunnel --no-autoupdate run --token eyJhIjoiZTAwMWM4MzlmMTNiNzU2ZDc0YWI1NjE0MzFlM2ZlZDciLCJ0IjoiNWM4MzdiNWMtODljYS00YzVmLTlhOWMtZGJkZDU5ODQwNjdjIiwicyI6Ik1HUm1aalJpTldRdE5URmhZeTAwTTJFekxUaGtNV1V0WWpCaU5tTTNORE0zT0RZdyJ9
 docker run -d --restart=always -p 15672:15672 -p 5672:5672 qics/sc:rabbitmq
 docker run -d --restart=always -e REDIS_PASSWORD=Lhnekj#_83jf -p 6379:6379 qics/sc:redis /bin/sh -c "redis-server --appendonly yes --requirepass ${REDIS_PASSWORD}"
-docker run -d --restart=always -e USER=qics -e PASSWD=Best12167 -p 5902:5902 -p 3389:3389 --privileged=true -v /D/backup/tmp:/tmp qics/deepin
+docker run -d --restart=always -e USER=qics -e PASSWD=Best12167 -p 5902:5902 -p 3388:3389 --privileged=true -v /D/backup/tmp:/tmp qics/deepin
 
