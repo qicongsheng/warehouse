e69a6cd912ad327cd1bb230b7bc62cc025950236
qicongsheng <875881559@qq.com>
2024-04-26 00:34:35 +0800

    gitaction

--- shell/docker/dockerfiles/drawio/drawio_builder.sh
+++ shell/docker/dockerfiles/drawio/drawio_builder.sh
@@ -5,3 +5,4 @@ mkdir /tmp/_drawio && cd /tmp/_drawio
 curl -o /tmp/_drawio/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/drawio/Dockerfile
 docker buildx build -t qics/drawio --platform linux/amd64,linux/arm64/v8 . --push
 cd /tmp && rm -fr /tmp/_drawio
+curl --location --request POST 'http://www.qics.cc/mail' --header 'Content-Type: application/json' --data '{"nick_name":"gitaction", "subject": "qics/drawio docker image pushed.", "content": "qics/drawio docker image pushed."}'
