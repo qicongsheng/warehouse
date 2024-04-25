f5d8879ecf15783a170e99ba034be317cbdeb8a8
qicongsheng <875881559@qq.com>
2024-04-26 00:11:35 +0800

    gitaction

--- shell/docker/dockerfiles/pickstar/pickstar_builder.sh
+++ shell/docker/dockerfiles/pickstar/pickstar_builder.sh
@@ -6,6 +6,8 @@ cd /tmp/_pickstar
 curl -o /tmp/_pickstar/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/pickstar/Dockerfile
 docker build -t qics/pickstar . --push
 cd /tmp && rm -fr /tmp/_pickstar
+curl --location --request POST 'http://www.qics.cc/mail' --header 'Content-Type: application/json' --data '{"subject": "gitaction tip","content": "qics/pickstar docker image pushed."}'
+
 
 
 
