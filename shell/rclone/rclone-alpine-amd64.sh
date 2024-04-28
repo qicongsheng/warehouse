00d75072fa213a54cd9d9bba6ce3fe8a21aa8cd2
qicongsheng <875881559@qq.com>
2024-04-28 18:02:11 +0800

    rclone

--- shell/rclone/rclone-alpine-amd64.sh
+++ shell/rclone/rclone-alpine-amd64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/rclone/rclone-alpine-amd64.sh" | /bin/sh
-apk update && apk add --no-cache curl fuse -y
+apk update && apk add --no-cache curl fuse
 mkdir -p /opt/rclone/onedriver && mkdir -p /onedriver
 curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone.conf https://www.qics.top/shell/docker/dockerfiles/rclone/rclone.conf
 curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone https://dpsdag.sn.files.1drv.com/y4mAfHVvWgMMCqDMAlc6tVKbVa0KSwyq1PCHwOZMLs_mn2BWpi6HQVdSbhfnbcAXGTXSE6X2X2PsSJK-LiW9L0sSlSMCMi09afc58gm1o_C_VFi6mNcEc5i-bC9-CttXYak9XFmbNaZJjNWOtRxVZe0C-vBJ2GFQT2r05cLqmUVr6Zhb9xXCFe5OdtIuZy9he8tq9vRlXgDcwCweVeOHjiuHw
