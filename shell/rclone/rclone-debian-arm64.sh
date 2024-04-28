3f5caf38a422a77812b3a06f6ddc42bae7eeeec7
qicongsheng <875881559@qq.com>
2024-04-28 19:10:04 +0800

    rclone

--- shell/rclone/rclone-debian-arm64.sh
+++ shell/rclone/rclone-debian-arm64.sh
@@ -3,7 +3,7 @@
 apt update && apt install curl fuse -y
 mkdir -p /opt/rclone/onedriver && mkdir -p /onedriver
 curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone.conf https://www.qics.top/shell/docker/dockerfiles/rclone/rclone.conf
-curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone https://dpsdag.sn.files.1drv.com/y4mtuZvCRqO9wjOYxaDdQkVNZiQs8k4kZCi1w-Lo0SGbtNafj7mPCXl83K1ZTPd7ER-hwEcFh69CkXzviDPlBvkAw7IjbDV59b-_H0mmvFPHlKx8oECrh8IasuLYw40d34ZrcrUkYmMJdkXY8PtW5JU1oH_YiMTRctTiqWodbYc_i70E3qVNHSVJtUQerTCb_YozXnUucF7ohibXZhjYEwJYw
+curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone https://dvuqua.sn.files.1drv.com/y4mK_Z3FuDaidv0ynxcjiClhkfxStCp6gpnb-HHnXo_I1_zRgHXeO31STOrvKqRJhGJ9lTka5tlmJbVC1AjWCJQgz0le9jMocmzqArcc0dr9pPjBx7rbRjmQ3dVDy_4meXg_Yt8wGKTf7fTJvX8aVZ-OU8GnKK6_r_bw36GIAnBj9JKJrE2HDx1XH6SH13fDrFqdjbxWyizXA8-T-dcKZRJ5Q
 chmod +x /opt/rclone/rclone
 ln -sf /opt/rclone/rclone /usr/local/bin/rclone
 ln -s /bin/fusermount /bin/fusermount3
