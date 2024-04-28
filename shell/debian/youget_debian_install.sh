c932d7336edb96e9b71e8289779fadca898b0310
qicongsheng <875881559@qq.com>
2024-04-29 00:47:03 +0800

    rclone

---
+++ shell/debian/youget_debian_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# =================================
+# curl -fsSL "https://www.qics.top/shell/debian/python39_install_debian_green.sh" | /bin/bash
+# curl -fsSL "https://www.qics.top/shell/debian/youget_debian_install.sh" | /bin/bash
+# curl -fsSL "https://www.qics.top/shell/rclone/rclone-debian-amd64.sh" | /bin/sh
+# =================================
+apt update && apt install ffmpeg -y
+pip3 install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple you-get
+
