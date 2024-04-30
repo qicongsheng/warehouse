edc30b07e30541509c9a9e41984272f001bc8dba
qicongsheng <875881559@qq.com>
2024-04-30 19:03:37 +0800

    docker

--- shell/docker/dockerfiles/work/sc/sc_builder.sh
+++ shell/docker/dockerfiles/work/sc/sc_builder.sh
@@ -10,7 +10,7 @@ cd /tmp && rm -fr /tmp/_rabbitmq
 
 mkdir /tmp/_redis && cd /tmp/_redis
 curl -o /tmp/_redis/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/work/sc/Dockerfile_work_sc_redis
-docker buildx build -t ghcr.io/qicongsheng/sc:rabbitmq --platform linux/amd64,linux/arm64/v8 . --push
+docker buildx build -t ghcr.io/qicongsheng/sc:redis --platform linux/amd64,linux/arm64/v8 . --push
 cd /tmp && rm -fr /tmp/_redis
 
 
