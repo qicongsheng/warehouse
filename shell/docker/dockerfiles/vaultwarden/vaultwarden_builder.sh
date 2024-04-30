3a8be5194fdd07b70a5bef70d409de5065b88f79
qicongsheng <875881559@qq.com>
2024-05-01 03:50:43 +0800

    docker

--- shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
+++ shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # curl -fsSL https://www.qics.top/shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh | /bin/sh
-IMG_NAME="vaultwarden"
+IMG_NAME="vaultwarden1"
 curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh
