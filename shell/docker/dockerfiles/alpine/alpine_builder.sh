bc5925a3c435fe2f985ceea0afcf5b4f5f399a6b
qicongsheng <875881559@qq.com>
2024-04-30 15:45:04 +0800

    docker

--- shell/docker/dockerfiles/alpine/alpine_builder.sh
+++ shell/docker/dockerfiles/alpine/alpine_builder.sh
@@ -5,6 +5,6 @@ curl -fsSL "https://www.qics.top/shell/docker/common/buildx_init.sh" | /bin/sh
 cd /tmp && rm -fr /tmp/_alpine
 mkdir /tmp/_alpine && cd /tmp/_alpine
 curl -o /tmp/_alpine/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/alpine/Dockerfile
-docker buildx build -t ghcr.io/qicongsheng/alpine --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t ghcr.io/qicongsheng/alpine --platform linux/amd64,linux/arm64/v8 . --push
 cd /tmp && rm -fr /tmp/_alpine
 curl --location --request POST 'http://www.qics.cc/mail' --header 'Content-Type: application/json' --data '{"nick_name":"gitaction", "subject": "qicongsheng/alpine docker image pushed.", "content": "qicongsheng/alpine docker image pushed."}'
