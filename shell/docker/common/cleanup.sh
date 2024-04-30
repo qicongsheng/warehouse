852b9b3f3af66805b7b99ec0efaf39127e0ed78e
qicongsheng <875881559@qq.com>
2024-05-01 04:12:28 +0800

    docker

--- shell/docker/common/cleanup.sh
+++ shell/docker/common/cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+IMG_NAME=$1
 cd /tmp && rm -fr /tmp/_$IMG_NAME
 curl --location --request POST 'http://www.qics.cc/mail' --header 'Content-Type: application/json' --data '{"nick_name":"gitaction", "subject": "qicongsheng/$IMG_NAME docker image pushed.", "content": "qicongsheng/$IMG_NAME docker image pushed."}'
 
--- shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
+++ shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
@@ -5,10 +5,11 @@ curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh -s $IMG_NAME
 
+cd /tmp/_$IMG_NAME
 curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/vaultwarden/Dockerfile
 docker buildx build -t ghcr.io/qicongsheng/$IMG_NAME --platform linux/amd64,linux/arm64 . --push
 
-curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh -s $IMG_NAME
 
 
 
