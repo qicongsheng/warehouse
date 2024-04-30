b2bf7aa5f114d9be72622d5e2d089953def7450d
qicongsheng <875881559@qq.com>
2024-05-01 04:09:42 +0800

    docker

--- shell/docker/common/tmpdir_init.sh
+++ shell/docker/common/tmpdir_init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+IMG_NAME=$1
 cd /tmp && rm -fr /tmp/_$IMG_NAME
 mkdir /tmp/_$IMG_NAME && cd /tmp/_$IMG_NAME
 
--- shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
+++ shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
@@ -3,7 +3,7 @@
 IMG_NAME="vaultwarden1"
 curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
-curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh -s $IMG_NAME
 
 curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/vaultwarden/Dockerfile
 docker buildx build -t ghcr.io/qicongsheng/$IMG_NAME --platform linux/amd64,linux/arm64 . --push
