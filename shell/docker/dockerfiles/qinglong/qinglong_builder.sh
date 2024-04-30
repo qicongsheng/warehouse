7d09f325ef9961027671a7c6a2f057341fcf4d2f
qicongsheng <875881559@qq.com>
2024-04-30 17:21:39 +0800

    docker

--- shell/docker/dockerfiles/qinglong/qinglong_builder.sh
+++ shell/docker/dockerfiles/qinglong/qinglong_builder.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/qinglong/qinglong_builder.sh" | /bin/sh
-docker buildx create --name mutibuilder
-docker buildx use mutibuilder
-docker buildx inspect --bootstrap
+curl -fsSL "https://www.qics.top/shell/docker/common/docker_login.sh" | /bin/sh
+curl -fsSL "https://www.qics.top/shell/docker/common/buildx_init.sh" | /bin/sh
 rm -fr /tmp/_qinglong
 mkdir /tmp/_qinglong
 cd /tmp/_qinglong
