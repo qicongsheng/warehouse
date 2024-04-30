8bb7fc053e749e8611224f2a9a3a1a5d4e94081a
qicongsheng <875881559@qq.com>
2024-05-01 03:37:25 +0800

    docker

--- shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
+++ shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/tinyproxy/tinyproxy_builder.sh | /bin/sh
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh | /bin/sh
 curl -fsSL "https://www.qics.top/shell/docker/common/docker_login.sh" | /bin/sh
 curl -fsSL "https://www.qics.top/shell/docker/common/buildx_init.sh" | /bin/sh
-cd /tmp && rm -fr /tmp/_tinyproxy
-mkdir /tmp/_tinyproxy && cd /tmp/_tinyproxy
-curl -o /tmp/_tinyproxy/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/tinyproxy/Dockerfile
-docker build -t ghcr.io/qicongsheng/tinyproxy . --push
-cd /tmp && rm -fr /tmp/_tinyproxy
+IMG_NAME="vaultwarden"
+cd /tmp && rm -fr /tmp/_$IMG_NAME
+mkdir /tmp/_$IMG_NAME && cd /tmp/_$IMG_NAME
+curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/vaultwarden/Dockerfile
+docker buildx build -t ghcr.io/qicongsheng/$IMG_NAME --platform linux/amd64,linux/arm64 . --push
+cd /tmp && rm -fr /tmp/_$IMG_NAME
 
 
 
