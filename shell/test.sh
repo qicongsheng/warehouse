5a7d491338a23e81056d6afb4e0e75feb436a309
qicongsheng <875881559@qq.com>
2024-05-02 00:55:47 +0800

    docker

---
+++ shell/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl -fsSL "https://www.qics.top/shell/docker/common/docker_login.sh" | /bin/sh
+#添加onedriver
+curl -fsSL "https://www.qics.top/shell/rclone/rclone-debian-arm64.sh" | /bin/sh
+ls /onedriver
+cp /onedriver/oracle12c.tar /tmp
+cd /tmp
+docker load < oracle12c.tar
+
+
+
