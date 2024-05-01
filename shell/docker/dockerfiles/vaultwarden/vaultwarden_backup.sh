ffbfcfd8d15b4b1c010f3800c9c775774851d3d0
qicongsheng <875881559@qq.com>
2024-05-01 18:48:53 +0800

    docker

--- shell/docker/dockerfiles/vaultwarden/vaultwarden_backup.sh
+++ shell/docker/dockerfiles/vaultwarden/vaultwarden_backup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # curl -fsSL https://www.qics.top/shell/docker/dockerfiles/vaultwarden/vaultwarden_backup.sh | /bin/bash
 export TZ=Asia/Shanghai
-DATE=$(date +"%a %b %e")
+DATE=$(date '+%Y%m%d%H%M%S')
 cd /data
-BACKUP_NAME="vaultwarden-$DATE.tar.gz vaultwarden"
+BACKUP_NAME="vaultwarden_$DATE.tar.gz"
 rm -fr $BACKUP_NAME
 rm -fr /onedriver/data/vaultwarden/$BACKUP_NAME
 tar -zcvf $BACKUP_NAME vaultwarden
+mkdir - p /onedriver/data/vaultwarden
 cp $BACKUP_NAME /onedriver/data/vaultwarden/
 rm -fr $BACKUP_NAME
 
