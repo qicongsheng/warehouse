d99d27ab64b780e3834b026c0c999419e3f7e10a
qicongsheng <875881559@qq.com>
2024-05-03 00:06:55 +0800

    docker

--- shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh
+++ shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 安装python3
 curl -fsSL https://www.qics.top/shell/debian/python39_install_debian_green.sh | /bin/bash
-pip3 install yagmail
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple yagmail
 
 rm -fr /opt/mail.py
 curl -o /opt/mail.py -L https://www.qics.top/shell/docker/common/mail.py
