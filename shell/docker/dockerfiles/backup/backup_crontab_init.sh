49fe5e67185007d17637fc6ff7c7c0e8a3e42400
qicongsheng <875881559@qq.com>
2024-05-03 00:52:17 +0800

    docker

--- shell/docker/dockerfiles/backup/backup_crontab_init.sh
+++ shell/docker/dockerfiles/backup/backup_crontab_init.sh
@@ -5,7 +5,7 @@ mkdir -p /var/spool/cron/crontabs
 crontab -r
 
 # 添加bitwarden备份定时任务
-echo "*/5 * * * * curl -fsSL http://www.qics.top/shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh | /bin/bash" >> /var/spool/cron/crontabs/root
+echo "30 1,13 * * * curl -fsSL http://www.qics.top/shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh | /bin/bash" >> /var/spool/cron/crontabs/root
 
 # 重启crontab服务
 chmod 600 /var/spool/cron/crontabs/root
