ab74ad5a2470e1f97b5e3cee4ab4a036e979c038
qicongsheng <875881559@qq.com>
2024-05-03 00:37:22 +0800

    docker

--- shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh
+++ shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo `date '+%Y-%m-%d %H:%M:%S'` "backup bitwarden check start..." >> /var/log/bitwarden_backup.log
 # 安装python3
 curl -fsSL https://www.qics.top/shell/debian/python39_install_debian_green.sh | /bin/bash
 pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple yagmail
 
+echo `date '+%Y-%m-%d %H:%M:%S'` "get mail.py..." >> /var/log/bitwarden_backup.log
 rm -fr /opt/mail.py
 curl -o /opt/mail.py -L https://www.qics.top/shell/docker/common/mail.py
 
@@ -11,10 +13,12 @@ DATE=$(date '+%Y%m%d_%H%M%S')
 cd /data
 BACKUP_NAME="vaultwarden_$DATE.tar.gz"
 rm -fr $BACKUP_NAME
+echo `date '+%Y-%m-%d %H:%M:%S'` "tar gz package..." >> /var/log/bitwarden_backup.log
 tar -zcvf $BACKUP_NAME vaultwarden
+echo `date '+%Y-%m-%d %H:%M:%S'` "send mail gz package..." >> /var/log/bitwarden_backup.log
 python3 /opt/mail.py $BACKUP_NAME "/data/$BACKUP_NAME"
 rm -fr $BACKUP_NAME
 rm -fr /opt/mail.py
-
+echo `date '+%Y-%m-%d %H:%M:%S'` "end..." >> /var/log/bitwarden_backup.log
 
 
