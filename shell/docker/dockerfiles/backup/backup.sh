3eda4ad32caa5a57611a7b8415d2715182d0fad3
qicongsheng <875881559@qq.com>
2024-05-02 20:05:21 +0800

    docker

---
+++ shell/docker/dockerfiles/backup/backup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "fuck" > /tmp/fuck.txt
+
---
+++ shell/docker/dockerfiles/backup/backup_builder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/debian/debian12_builder.sh | /bin/sh
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/pickstar/pickstar_builder.sh | /bin/sh
+IMG_NAME="debian"
+
+curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh -s $IMG_NAME
+cd /tmp/_$IMG_NAME
+
+curl -o /tmp/_debian12/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
+docker buildx build -t ghcr.io/qicongsheng/debian --platform linux/amd64,linux/arm64 . --push
+
+curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh -s $IMG_NAME
+
+
+
