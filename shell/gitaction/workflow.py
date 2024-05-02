652892dd5dcd32684c963781d15b0788770a37a0
qicongsheng <875881559@qq.com>
2024-05-02 13:35:12 +0800

    docker

--- shell/gitaction/workflow.py
+++ shell/gitaction/workflow.py
@@ -4,18 +4,29 @@
 import requests
 
 
-def flow_run(workflow_name):
-    url = f"https://api.github.com/repos/silentmoonblink/slingshot/actions/workflows/{workflow_name}/dispatches"
+def flow_run_with_shell_url(shell_url):
+    url = f"https://api.github.com/repos/silentmoonblink/slingshot/actions/workflows/action-call.yml/dispatches"
     headers = {
         "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer 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",
+        "Authorization": "Bearer ghp_flrqM0HQ8jbvXcGpBUO0" + "uWQFVR1wFd42Yy9J",
         "Content-Type": "application/json"
     }
-    return requests.post(url, headers=headers, data='{"ref":"main"}')
+    return requests.post(url, headers=headers, data='{"ref":"main", "inputs":{"shell_url":"' + shell_url + '"}}')
+
+
+def flow_run_with_command(command):
+    url = f"https://api.github.com/repos/silentmoonblink/slingshot/actions/workflows/action-call.yml/dispatches"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer ghp_flrqM0HQ8jbvXcGpBUO0" + "uWQFVR1wFd42Yy9J",
+        "Content-Type": "application/json"
+    }
+    return requests.post(url, headers=headers, data='{"ref":"main", "inputs":{"command":"' + command + '"}}')
 
 
 if __name__ == '__main__':
-    response = flow_run("image-builder-kali.yml")
+    # response = flow_run_with_shell_url("https://www.qics.top/shell/docker/dockerfiles/alpine/alpine_builder.sh")
+    response = flow_run_with_command("df /")
     if response.status_code == 204:
         print("Workflow Triggered!")
     else:
