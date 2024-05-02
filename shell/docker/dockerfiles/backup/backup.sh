4ec7821e401f2016fdcd4e61b9b4800710585de9
qicongsheng <875881559@qq.com>
2024-05-02 22:32:37 +0800

    docker

--- shell/docker/dockerfiles/backup/backup.sh
+++ shell/docker/dockerfiles/backup/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 安装python3
-curl -fsSL "https://www.qics.top/shell/debian/python39_install_debian_green.sh" | /bin/bash
+curl -fsSL https://www.qics.top/shell/debian/python39_install_debian_green.sh | /bin/bash
 
 
