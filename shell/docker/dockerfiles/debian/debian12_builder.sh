aaf41116410eb15cb87e5be01e3ff864f60e249a
qicongsheng <875881559@qq.com>
2024-05-02 19:18:40 +0800

    docker

--- shell/docker/dockerfiles/debian/debian12_builder.sh
+++ shell/docker/dockerfiles/debian/debian12_builder.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/debian/debian12_builder.sh" | /bin/sh
-# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/pickstar/pickstar_builder.sh" | /bin/sh
-curl -fsSL "https://www.qics.top/shell/docker/common/docker_login.sh" | /bin/sh
-curl -fsSL "https://www.qics.top/shell/docker/common/buildx_init.sh" | /bin/sh
-rm -fr /tmp/_debian12
-mkdir /tmp/_debian12 && cd /tmp/_debian12
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/debian/debian12_builder.sh | /bin/sh
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/pickstar/pickstar_builder.sh | /bin/sh
+IMG_NAME="debian"
+
+curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh -s $IMG_NAME
+cd /tmp/_$IMG_NAME
+
 curl -o /tmp/_debian12/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/debian/Dockerfile_debian12
 docker buildx build -t ghcr.io/qicongsheng/debian --platform linux/amd64,linux/arm64 . --push
-cd /tmp && rm -fr /tmp/_debian12
-curl --location --request POST 'http://www.qics.cc/mail' --header 'Content-Type: application/json' --data '{"nick_name":"gitaction", "subject": "ghcr.io/qicongsheng/debian docker image pushed.", "content": "ghcr.io/qicongsheng/debian docker image pushed."}'
+
+curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh -s $IMG_NAME
+
 
 
