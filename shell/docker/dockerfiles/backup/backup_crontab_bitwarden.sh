44d9c9d1923ac1b0818907ff690b67e48cb681a7
qicongsheng <875881559@qq.com>
2024-05-04 00:25:31 +0800

    docker

--- shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh
+++ shell/docker/dockerfiles/backup/backup_crontab_bitwarden.sh
@@ -17,8 +17,17 @@ BACKUP_NAME="vaultwarden_$DATE.tar.gz"
 rm -fr $BACKUP_NAME
 echo `date '+%Y-%m-%d %H:%M:%S'` "tar $BACKUP_NAME package..." >> /var/log/bitwarden_backup.log
 tar -zcvf $BACKUP_NAME vaultwarden
+
+# 备份到邮件
 echo `date '+%Y-%m-%d %H:%M:%S'` "send mail $BACKUP_NAME package..." >> /var/log/bitwarden_backup.log
 python3 /opt/mail.py "bitwarden" "bitwarden backup[$BACKUP_NAME]" "/data/$BACKUP_NAME"
+
+# 备份到onedriver
+echo `date '+%Y-%m-%d %H:%M:%S'` "upload onedriver $BACKUP_NAME package..." >> /var/log/bitwarden_backup.log
+rm -fr /onedriver/backup/vaultwarden/vaultwarden.tar.gz
+cp $BACKUP_NAME /onedriver/backup/vaultwarden
+mv /onedriver/backup/vaultwarden/$BACKUP_NAME vaultwarden.tar.gz
+
 rm -fr $BACKUP_NAME
 rm -fr /opt/mail.py
 echo `date '+%Y-%m-%d %H:%M:%S'` "end..." >> /var/log/bitwarden_backup.log
