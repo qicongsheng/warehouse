cf6bac9a3793925e36d8a85ad4a61b4943557554
qicongsheng <875881559@qq.com>
2024-05-04 02:09:55 +0800

    docker

--- shell/vps/tencent_init.sh
+++ shell/vps/tencent_init.sh
@@ -49,7 +49,7 @@ docker rmi ghcr.io/qicongsheng/shellngn
 docker run -d --restart=always -p 9000:80 --privileged=true -v /tmp:/etc/nginx/html ghcr.nju.edu.cn/qicongsheng/nginx
 docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123# ghcr.nju.edu.cn/qicongsheng/mysql:5.7
 docker run -d --restart=always -e HOST=0.0.0.0 -e PORT=8066 --net=host ghcr.nju.edu.cn/qicongsheng/shellngn
-docker run -d --restart=always -p 8081:8081 ghcr.nju.edu.cn/qicongsheng/nexus
+docker run -d --restart=always -p 8081:8081 ghcr.nju.edu.cn/qicongsheng/nexus:dev
 docker run -d --restart=always -p 5212:5212 --privileged=true -v /tmp:/tmp ghcr.nju.edu.cn/qicongsheng/cloudreve
 docker run -d --restart=always -p 5700:5700 ghcr.nju.edu.cn/qicongsheng/qinglong
 docker run -d --restart=always --net=host ghcr.nju.edu.cn/qicongsheng/sc:rabbitmq
