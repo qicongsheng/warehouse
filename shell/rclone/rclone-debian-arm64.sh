c1522a375b692fb5623015322dfe1fbef3fea07b
qicongsheng <875881559@qq.com>
2024-05-04 00:29:37 +0800

    docker

--- shell/rclone/rclone-debian-arm64.sh
+++ shell/rclone/rclone-debian-arm64.sh
@@ -3,11 +3,14 @@
 # =================================
 # 解除挂载 fusermount -u /onedriver
 # =================================
-apt update && apt install curl fuse -y
-mkdir -p /opt/rclone/onedriver && mkdir -p /onedriver
-curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone.conf https://www.qics.top/shell/docker/dockerfiles/rclone/rclone.conf
-curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone https://dvuqua.sn.files.1drv.com/y4mK_Z3FuDaidv0ynxcjiClhkfxStCp6gpnb-HHnXo_I1_zRgHXeO31STOrvKqRJhGJ9lTka5tlmJbVC1AjWCJQgz0le9jMocmzqArcc0dr9pPjBx7rbRjmQ3dVDy_4meXg_Yt8wGKTf7fTJvX8aVZ-OU8GnKK6_r_bw36GIAnBj9JKJrE2HDx1XH6SH13fDrFqdjbxWyizXA8-T-dcKZRJ5Q
-chmod +x /opt/rclone/rclone
-ln -sf /opt/rclone/rclone /usr/local/bin/rclone
-ln -s /bin/fusermount /bin/fusermount3
-nohup rclone --config /opt/rclone/rclone.conf mount onedriver:/rclone/data /onedriver --copy-links --no-gzip-encoding --no-check-certificate --allow-other --allow-non-empty --umask 000 > /var/log/rclone.log 2>&1 &
+if [ ! "$(command -v rclone)" ]; then
+    echo "rclone 没有安装" >&2
+    apt update && apt install curl fuse -y
+    mkdir -p /opt/rclone/onedriver && mkdir -p /onedriver
+    curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone.conf https://www.qics.top/shell/docker/dockerfiles/rclone/rclone.conf
+    curl --connect-timeout 600 -m 7200 --continue-at - -o /opt/rclone/rclone https://dvuqua.sn.files.1drv.com/y4mK_Z3FuDaidv0ynxcjiClhkfxStCp6gpnb-HHnXo_I1_zRgHXeO31STOrvKqRJhGJ9lTka5tlmJbVC1AjWCJQgz0le9jMocmzqArcc0dr9pPjBx7rbRjmQ3dVDy_4meXg_Yt8wGKTf7fTJvX8aVZ-OU8GnKK6_r_bw36GIAnBj9JKJrE2HDx1XH6SH13fDrFqdjbxWyizXA8-T-dcKZRJ5Q
+    chmod +x /opt/rclone/rclone
+    ln -sf /opt/rclone/rclone /usr/local/bin/rclone
+    ln -s /bin/fusermount /bin/fusermount3
+    nohup rclone --config /opt/rclone/rclone.conf mount onedriver:/rclone/data /onedriver --copy-links --no-gzip-encoding --no-check-certificate --allow-other --allow-non-empty --umask 000 > /var/log/rclone.log 2>&1 &
+fi
