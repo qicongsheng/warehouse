fd2f2db5d17249f98308af644fcdf4cacc1bcaef
qicongsheng <875881559@qq.com>
2024-05-05 18:25:47 +0800

    docker

---
+++ shell/docker/dockerfiles/nascab/nascab_builder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/nascab/nascab_builder.sh" | /bin/sh
+IMG_NAME="nascab"
+
+curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh -s $IMG_NAME
+cd /tmp/_$IMG_NAME
+
+curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/nascab/Dockerfile
+docker buildx build -t ghcr.io/qicongsheng/$IMG_NAME --platform linux/amd64,linux/arm64 . --push
+
+curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh -s $IMG_NAME
+
+
+
