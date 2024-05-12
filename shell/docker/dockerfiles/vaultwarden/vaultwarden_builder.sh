35ebb78717546fe241709986eeb809c3c2ab161e
qicongsheng <875881559@qq.com>
2024-05-12 17:15:46 +0800

    docker

--- shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
+++ shell/docker/dockerfiles/vaultwarden/vaultwarden_builder.sh
@@ -8,7 +8,7 @@ curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh -s 
 cd /tmp/_$IMG_NAME
 
 curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/vaultwarden/Dockerfile
-docker buildx build -t ghcr.io/qicongsheng/$IMG_NAME --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t ghcr.io/qicongsheng/vaultwarden:alpha --platform linux/amd64,linux/arm64 . --push
 
 curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh -s $IMG_NAME
 
