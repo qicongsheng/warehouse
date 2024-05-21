a6810d684589f2687ccf89df9415f4bfa427b5f5
qicongsheng <875881559@qq.com>
2024-05-21 11:30:50 +0800

    webdav

--- shell/docker/dockerfiles/mysql/mysql57_builder.sh
+++ shell/docker/dockerfiles/mysql/mysql57_builder.sh
@@ -9,7 +9,7 @@ cd /tmp/_$IMG_NAME
 
 curl -o /tmp/_$IMG_NAME/docker-entrypoint.sh -L https://www.qics.top/shell/docker/dockerfiles/mysql/5.7/docker-entrypoint.sh
 curl -o /tmp/_$IMG_NAME/healthcheck.sh -L https://www.qics.top/shell/docker/dockerfiles/mysql/5.7/healthcheck.sh
-curl -o /tmp/_$IMG_NAME/allow_remote_login.sql -L https://www.qics.top/shell/docker/dockerfiles/mysql/5.7/allow_remote_login.sql
+curl -o /tmp/_$IMG_NAME/init_setting.sql -L https://www.qics.top/shell/docker/dockerfiles/mysql/5.7/init_setting.sql
 curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/mysql/5.7/Dockerfile
 
 chmod +x /tmp/_$IMG_NAME/docker-entrypoint.sh
