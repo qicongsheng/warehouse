51a72298a3bc64a99b18a439ca7b5e5081e8ee17
qicongsheng <875881559@qq.com>
2024-05-29 18:25:01 +0800

    webdav

--- shell/vps/win10_init.sh
+++ shell/vps/win10_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run -d --restart=always -p 8096:8096 ghcr.nju.edu.cn/qicongsheng/jellyfin
 docker run -d --restart=always -p 9000:80 -v /d/backup:/etc/nginx/html ghcr.nju.edu.cn/qicongsheng/nginx
+docker run -d --restart=always -e API_KEY=LAdoeJAuaq_uB2kCblxZDtF5wJFrDYHQfSHOFge0 -e ZONE=qics.top -e SUBDOMAIN=home ghcr.nju.edu.cn/qicongsheng/ddns
 docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123# ghcr.nju.edu.cn/qicongsheng/mysql:5.7
 docker run -d --restart=always --net=host ghcr.nju.edu.cn/qicongsheng/cloudflared tunnel --no-autoupdate run --token eyJhIjoiZTAwMWM4MzlmMTNiNzU2ZDc0YWI1NjE0MzFlM2ZlZDciLCJ0IjoiNWM4MzdiNWMtODljYS00YzVmLTlhOWMtZGJkZDU5ODQwNjdjIiwicyI6Ik1HUm1aalJpTldRdE5URmhZeTAwTTJFekxUaGtNV1V0WWpCaU5tTTNORE0zT0RZdyJ9
 docker run -d --restart=always -p 15672:15672 -p 5672:5672 ghcr.nju.edu.cn/qicongsheng/sc:rabbitmq
