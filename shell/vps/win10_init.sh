5f8cc20a17b914b95a35b999e14ec9fede797a67
qicongsheng <875881559@qq.com>
2024-05-29 18:24:14 +0800

    webdav

--- shell/vps/win10_init.sh
+++ shell/vps/win10_init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker run -d --restart=always -p 8096:8096 ghcr.io/qicongsheng/jellyfin
-docker run -d --restart=always -p 9000:80 -v /d/backup:/etc/nginx/html ghcr.io/qicongsheng/nginx
-docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123# ghcr.io/qicongsheng/mysql:5.7
-docker run -d --restart=always --net=host ghcr.io/qicongsheng/cloudflared tunnel --no-autoupdate run --token eyJhIjoiZTAwMWM4MzlmMTNiNzU2ZDc0YWI1NjE0MzFlM2ZlZDciLCJ0IjoiNWM4MzdiNWMtODljYS00YzVmLTlhOWMtZGJkZDU5ODQwNjdjIiwicyI6Ik1HUm1aalJpTldRdE5URmhZeTAwTTJFekxUaGtNV1V0WWpCaU5tTTNORE0zT0RZdyJ9
-docker run -d --restart=always -p 15672:15672 -p 5672:5672 ghcr.io/qicongsheng/sc:rabbitmq
-docker run -d --restart=always -e REDIS_PASSWORD=Lhnekj#_83jf -p 6379:6379 ghcr.io/qicongsheng/sc:redis /bin/sh -c "redis-server --appendonly yes --requirepass ${REDIS_PASSWORD}"
-docker run -d --restart=always -e USER=qics -e PASSWD=Best12167 -p 5902:5902 -p 3388:3389 ghcr.io/qicongsheng/deepin
+docker run -d --restart=always -p 8096:8096 ghcr.nju.edu.cn/qicongsheng/jellyfin
+docker run -d --restart=always -p 9000:80 -v /d/backup:/etc/nginx/html ghcr.nju.edu.cn/qicongsheng/nginx
+docker run -d --restart=always -p 3306:3306 -e MYSQL_ROOT_PASSWORD=youarebadboy123# ghcr.nju.edu.cn/qicongsheng/mysql:5.7
+docker run -d --restart=always --net=host ghcr.nju.edu.cn/qicongsheng/cloudflared tunnel --no-autoupdate run --token eyJhIjoiZTAwMWM4MzlmMTNiNzU2ZDc0YWI1NjE0MzFlM2ZlZDciLCJ0IjoiNWM4MzdiNWMtODljYS00YzVmLTlhOWMtZGJkZDU5ODQwNjdjIiwicyI6Ik1HUm1aalJpTldRdE5URmhZeTAwTTJFekxUaGtNV1V0WWpCaU5tTTNORE0zT0RZdyJ9
+docker run -d --restart=always -p 15672:15672 -p 5672:5672 ghcr.nju.edu.cn/qicongsheng/sc:rabbitmq
+docker run -d --restart=always -e REDIS_PASSWORD=Lhnekj#_83jf -p 6379:6379 ghcr.nju.edu.cn/qicongsheng/sc:redis /bin/sh -c "redis-server --appendonly yes --requirepass ${REDIS_PASSWORD}"
+docker run -d --restart=always -e USER=qics -e PASSWD=Best12167 -p 5902:5902 -p 3388:3389 ghcr.nju.edu.cn/qicongsheng/deepin
 
