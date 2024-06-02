ba2f2be13421540c379cfff6c0729e9ef4256a25
qicongsheng <875881559@qq.com>
2024-06-03 00:34:10 +0800

    webdav

--- shell/docker/common/docker_login.sh
+++ shell/docker/common/docker_login.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-part1="ghp_s4XbjIIUX0yb0GyG"
-part2="KMf5GAonGBVNaF2nSLJF"
+part1="ghp_Fl7ljrxlhdyYSndg"
+part2="5AheaV9yMgH74k3OAABU"
 echo "${part1}${part2}" | docker login ghcr.io -u qicongsheng --password-stdin
