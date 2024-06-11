66268d2e607f1ebeecd7bd7a461fec82ba766364
qicongsheng <875881559@qq.com>
2024-06-11 20:46:18 +0800

    webdav

--- shell/docker/dockerfiles/nascab/nascab_builder.sh
+++ shell/docker/dockerfiles/nascab/nascab_builder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/nascab/nascab_builder.sh" | /bin/sh
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/nascab/nascab_builder.sh | /bin/sh
 IMG_NAME="nascab"
 
 curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
@@ -9,6 +9,7 @@ cd /tmp/_$IMG_NAME
 
 curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/nascab/Dockerfile
 docker buildx build -t ghcr.io/qicongsheng/$IMG_NAME --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME --platform linux/amd64,linux/arm64 . --push
 
 curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh -s $IMG_NAME
 
