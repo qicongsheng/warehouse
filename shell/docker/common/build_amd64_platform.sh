783671aa151305577279522231abd763969066c2
qicongsheng <875881559@qq.com>
2024-06-12 00:42:36 +0800

    webdav

--- shell/docker/common/build_amd64_platform.sh
+++ shell/docker/common/build_amd64_platform.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 IMG_FULL_NAME=$1
-docker buildx build -t ghcr.io/qicongsheng/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
-docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
-docker buildx build -t tc.qics.top:8000/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
-
+docker build -t ghcr.io/qicongsheng/$IMG_FULL_NAME . --push
+docker build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME . --push
+docker build -t tc.qics.top:8000/$IMG_FULL_NAME . --push
