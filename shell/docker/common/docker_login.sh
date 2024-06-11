3563559bbfb58a033ace9c78c0dab262654e8969
qicongsheng <875881559@qq.com>
2024-06-12 00:21:03 +0800

    webdav

--- shell/docker/common/docker_login.sh
+++ shell/docker/common/docker_login.sh
@@ -3,3 +3,4 @@ part1="ghp_Fl7ljrxlhdyYSndg"
 part2="5AheaV9yMgH74k3OAABU"
 echo "${part1}${part2}" | docker login ghcr.io -u qicongsheng --password-stdin
 echo "Best12167" | docker login --username=875881559@qq.com registry.cn-hangzhou.aliyuncs.com --password-stdin
+echo "Best12167" | docker login --username=qicongsheng tc.qics.top:8000 --password-stdin
