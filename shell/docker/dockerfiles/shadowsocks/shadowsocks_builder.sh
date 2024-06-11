9a92a055c4fbdeee3bdfc54319b28462ffe110d7
qicongsheng <875881559@qq.com>
2024-06-12 03:30:17 +0800

    webdav

--- shell/docker/dockerfiles/shadowsocks/shadowsocks_builder.sh
+++ shell/docker/dockerfiles/shadowsocks/shadowsocks_builder.sh
@@ -5,7 +5,6 @@ curl -fsSL "https://www.qics.top/shell/docker/common/buildx_init.sh" | /bin/sh
 cd /tmp && rm -fr /tmp/_shadowsocks
 mkdir /tmp/_shadowsocks && cd /tmp/_shadowsocks
 curl -o /tmp/_shadowsocks/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/shadowsocks/Dockerfile_shadowsocks_server
-docker buildx build -t ghcr.io/qicongsheng/shadowsocks:server --platform linux/amd64,linux/arm64 . --push
-docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/shadowsocks:server --platform linux/amd64,linux/arm64 . --push
+curl -fsSL https://www.qics.top/shell/docker/common/build_cross_platform.sh | /bin/sh -s "shadowsocks:server"
 cd /tmp && rm -fr /tmp/_shadowsocks
 curl --location --request POST 'http://www.qics.cc/mail' --header 'Content-Type: application/json' --data '{"nick_name":"gitaction", "subject": "qicongsheng/shadowsocks docker image pushed.", "content": "qicongsheng/shadowsocks docker image pushed."}'
