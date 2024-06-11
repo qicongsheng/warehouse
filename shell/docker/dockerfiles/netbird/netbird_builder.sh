72f9d9529c72efdd81b6a4d9b8b1f669ee280b58
qicongsheng <875881559@qq.com>
2024-06-11 20:18:39 +0800

    webdav

--- shell/docker/dockerfiles/netbird/netbird_builder.sh
+++ shell/docker/dockerfiles/netbird/netbird_builder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# curl -fsSL "https://www.qics.top/shell/docker/dockerfiles/netbird/netbird_builder.sh" | /bin/sh
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/netbird/netbird_builder.sh | /bin/sh
 curl -fsSL "https://www.qics.top/shell/docker/common/docker_login.sh" | /bin/sh
 curl -fsSL "https://www.qics.top/shell/docker/common/buildx_init.sh" | /bin/sh
 rm -fr /tmp/_netbird
