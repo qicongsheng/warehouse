d131e1f1417656f6ab8517fa6c7cf3901c6917d8
qicongsheng <875881559@qq.com>
2024-06-12 18:01:26 +0800

    webdav

--- shell/docker/dockerfiles/backup/lenovo/backup_builder.sh
+++ shell/docker/dockerfiles/backup/lenovo/backup_builder.sh
@@ -8,8 +8,7 @@ curl -fsSL https://www.qics.top/shell/docker/common/tmpdir_init.sh | /bin/sh -s 
 cd /tmp/_$IMG_NAME
 
 curl -o /tmp/_$IMG_NAME/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/lenovo/tencent/Dockerfile
-docker buildx build -t ghcr.io/qicongsheng/backup:lenovo --platform linux/amd64,linux/arm64 . --push
-docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/backup:lenovo --platform linux/amd64,linux/arm64 . --push
+curl -fsSL https://www.qics.top/shell/docker/common/build_cross_platform.sh | /bin/sh -s "backup:lenovo"
 
 curl -fsSL https://www.qics.top/shell/docker/common/cleanup.sh | /bin/sh -s $IMG_NAME
 
