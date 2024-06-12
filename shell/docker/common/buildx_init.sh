1b70886b42e7abd4906aeccfa2bb96bf71178e13
qicongsheng <875881559@qq.com>
2024-06-12 15:41:42 +0800

    webdav

--- shell/docker/common/buildx_init.sh
+++ shell/docker/common/buildx_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-docker buildx create --name mutibuilder
+curl -o /tmp/buildkitd.toml -L https://www.qics.top/shell/docker/common/buildkitd.toml
+docker buildx create --name mutibuilder --config=/tmp/buildkitd.toml
 docker buildx use mutibuilder
 docker buildx inspect --bootstrap
 
--- shell/docker/common/docker_login.sh
+++ shell/docker/common/docker_login.sh
@@ -5,7 +5,7 @@ part2="5AheaV9yMgH74k3OAABU"
 echo "${part1}${part2}" | docker login ghcr.io -u qicongsheng --password-stdin
 echo "Best12167" | docker login --username=875881559@qq.com registry.cn-hangzhou.aliyuncs.com --password-stdin
 echo "Best12167" | docker login --username=qicongsheng registry.qics.top --password-stdin
-echo "Best12167" | docker login --username=qicongsheng tc.qics.top:8000 --password-stdin
+echo "Best12167" | docker login --username=qicongsheng 42.192.22.70:8000 --password-stdin
 
 
 
