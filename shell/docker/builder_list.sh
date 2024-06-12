0c006568b7ff1f3228364ccef52bb4b0b3b2b487
qicongsheng <875881559@qq.com>
2024-06-12 20:31:46 +0800

    webdav

--- shell/docker/builder_list.sh
+++ shell/docker/builder_list.sh
@@ -36,3 +36,4 @@ curl -fsSL https://www.qics.top/shell/docker/dockerfiles/webdav/webdav_builder.s
 curl -fsSL https://www.qics.top/shell/docker/dockerfiles/webshell/webshell_builder.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/dockerfiles/work/sc/sc_builder.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/dockerfiles/other/other_builder.sh | /bin/sh
+
