b2c66adac768f7f7d9957a8771a51d7645e1e007
qicongsheng <875881559@qq.com>
2024-06-12 10:44:58 +0800

    webdav

--- shell/docker/common/build_amd64_platform.sh
+++ shell/docker/common/build_amd64_platform.sh
@@ -2,4 +2,4 @@
 IMG_FULL_NAME=$1
 docker build -t ghcr.io/qicongsheng/$IMG_FULL_NAME . --push
 docker build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME . --push
-docker build -t 42.192.22.70:8000/$IMG_FULL_NAME . --push
+docker build -t registry.qics.top/$IMG_FULL_NAME . --push
--- shell/docker/common/build_cross_platform.sh
+++ shell/docker/common/build_cross_platform.sh
@@ -2,5 +2,5 @@
 IMG_FULL_NAME=$1
 docker buildx build -t ghcr.io/qicongsheng/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
 docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
-docker buildx build -t 42.192.22.70:8000/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t registry.qics.top/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
 
