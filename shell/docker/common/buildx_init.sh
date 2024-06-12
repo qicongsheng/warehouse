2b976ca75f2bd328775c35a0c0acf84a2cb0ec01
qicongsheng <875881559@qq.com>
2024-06-12 19:13:25 +0800

    webdav

--- shell/docker/common/buildx_init.sh
+++ shell/docker/common/buildx_init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# container path => /etc/buildkit/buildkitd.toml
 curl -o /tmp/buildkitd.toml -L https://www.qics.top/shell/docker/common/buildkitd.toml
 docker buildx create --name mutibuilder --config=/tmp/buildkitd.toml
 docker buildx use mutibuilder
