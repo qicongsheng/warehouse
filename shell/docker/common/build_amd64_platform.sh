f32d628e6945d1cc0710c69419d00604317c175e
qicongsheng <875881559@qq.com>
2024-06-13 21:17:43 +0800

    webdav

--- shell/docker/common/build_amd64_platform.sh
+++ shell/docker/common/build_amd64_platform.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 IMG_FULL_NAME=$1
-docker build -t ghcr.io/qicongsheng/$IMG_FULL_NAME_amd64 . --push
-docker pull ghcr.io/qicongsheng/$IMG_FULL_NAME_amd64
-docker manifest create ghcr.io/qicongsheng/$IMG_FULL_NAME ghcr.io/qicongsheng/$IMG_FULL_NAME_amd64
+IMG_FULL_NAME_PLATFORM="$IMG_FULL_NAME_amd64"
+docker build -t ghcr.io/qicongsheng/IMG_FULL_NAME_PLATFORM . --push
+docker pull ghcr.io/qicongsheng/IMG_FULL_NAME_PLATFORM
+docker manifest create ghcr.io/qicongsheng/$IMG_FULL_NAME ghcr.io/qicongsheng/IMG_FULL_NAME_PLATFORM
 docker manifest push ghcr.io/qicongsheng/$IMG_FULL_NAME
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_amd64 . --push
-docker pull registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_amd64
-docker manifest create registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME ghcr.io/qicongsheng/$IMG_FULL_NAME_amd64
+docker build -t registry.cn-hangzhou.aliyuncs.com/qics/IMG_FULL_NAME_PLATFORM . --push
+docker pull registry.cn-hangzhou.aliyuncs.com/qics/IMG_FULL_NAME_PLATFORM
+docker manifest create registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME ghcr.io/qicongsheng/IMG_FULL_NAME_PLATFORM
 docker manifest push registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME
 
