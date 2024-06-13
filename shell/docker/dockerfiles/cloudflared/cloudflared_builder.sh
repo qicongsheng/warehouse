01774bed7fa49f6a6eac33367d9c5dd0f4b01007
qicongsheng <875881559@qq.com>
2024-06-14 03:01:35 +0800

    webdav

--- shell/docker/dockerfiles/cloudflared/cloudflared_builder.sh
+++ shell/docker/dockerfiles/cloudflared/cloudflared_builder.sh
@@ -5,6 +5,6 @@ curl -fsSL "https://www.qics.top/shell/docker/common/buildx_init.sh" | /bin/sh
 cd /tmp && rm -fr /tmp/_cloudflared
 mkdir /tmp/_cloudflared && cd /tmp/_cloudflared
 curl -o /tmp/_cloudflared/Dockerfile -L https://www.qics.top/shell/docker/dockerfiles/cloudflared/Dockerfile
-curl -fsSL https://www.qics.top/shell/docker/common/build_cross_platform.sh | /bin/sh -s "cloudflared"
+curl -fsSL https://www.qics.top/shell/docker/common/build_cross_platform.sh | /bin/sh -s "cloudflared" "latest"
 cd /tmp && rm -fr /tmp/_cloudflared
 curl --location --request POST 'http://www.qics.cc/mail' --header 'Content-Type: application/json' --data '{"nick_name":"gitaction", "subject": "ghcr.io/qicongsheng/cloudflared docker image pushed.", "content": "ghcr.io/qicongsheng/cloudflared docker image pushed."}'
