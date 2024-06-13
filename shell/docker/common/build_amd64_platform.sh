174b76b24c9881fb1a252b1641af0165b4454eb8
qicongsheng <875881559@qq.com>
2024-06-13 22:01:02 +0800

    webdav

--- shell/docker/common/build_amd64_platform.sh
+++ shell/docker/common/build_amd64_platform.sh
@@ -10,5 +10,5 @@ docker manifest push ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION
 
 docker build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM . --push
 docker pull registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM
-docker manifest create registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME:$IMG_VERSION ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM
+docker manifest create registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME:$IMG_VERSION registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM
 docker manifest push registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME:$IMG_VERSION
