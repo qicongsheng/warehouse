1f9fca9dac5cf14f19b72f5d517abf02e48d4118
qicongsheng <875881559@qq.com>
2024-06-14 00:39:38 +0800

    webdav

--- shell/docker/common/build_cross_platform.sh
+++ shell/docker/common/build_cross_platform.sh
@@ -2,5 +2,6 @@
 IMG_FULL_NAME=$1
 docker buildx build -t ghcr.io/qicongsheng/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
 docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t ccr.ccs.tencentyun.com/qicongsheng/$IMG_FULL_NAME --platform linux/amd64,linux/arm64 . --push
 
 
--- shell/docker/common/docker_login.sh
+++ shell/docker/common/docker_login.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 part1="ghp_Fl7ljrxlhdyYSndg"
 part2="5AheaV9yMgH74k3OAABU"
-echo "${part1}${part2}" | docker login ghcr.io -u qicongsheng --password-stdin
-echo "Best12167" | docker login --username=875881559@qq.com registry.cn-hangzhou.aliyuncs.com --password-stdin
-echo "Best12167" | docker login --username=qicongsheng registry.qics.top --password-stdin
-echo "Best12167" | docker login --username=qicongsheng 42.192.22.70:8000 --password-stdin
-
-
-
+echo "${part1}${part2}" | docker login ghcr.io --username=qicongsheng --password-stdin
+echo "Best12167" | docker login registry.cn-hangzhou.aliyuncs.com --username=875881559@qq.com --password-stdin
+echo "Best12167" | docker login ccr.ccs.tencentyun.com --username=qicongsheng --password-stdin
