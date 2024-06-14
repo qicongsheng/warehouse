3745404ef9b5c317c491db29453b8ab6cdb17011
qicongsheng <875881559@qq.com>
2024-06-14 14:31:00 +0800

    webdav

--- shell/docker/sync/syncer.sh
+++ shell/docker/sync/syncer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# curl -fsSL https://www.qics.top/shell/docker/syncer.sh | /bin/sh
+# curl -fsSL https://www.qics.top/shell/docker/sync/syncer.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
 REGISTRY_SOURCE="ghcr.io/qicongsheng"
