29101c71dba5ccbdc4f81f8117cc54463d775f12
qicongsheng <875881559@qq.com>
2024-06-14 13:08:53 +0800

    webdav

---
+++ shell/docker/sync_list.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# curl -fsSL https://www.qics.top/shell/docker/dockerfiles/sync_list.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
+
+REGISTRY_SOURCE="ghcr.io/qicongsheng"
+REGISTRY_TARGET="registry.cn-hangzhou.aliyuncs.com/qics"
+
+curl -fsSL https://www.qics.top/shell/docker/common/img_sync.sh | /bin/sh -s $REGISTRY_SOURCE $REGISTRY_TARGET "alist" "latest"
+
+
