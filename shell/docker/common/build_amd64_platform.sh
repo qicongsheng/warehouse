617697f979a9005a5cc3d532e58224451fa6eabf
qicongsheng <875881559@qq.com>
2024-06-14 13:53:08 +0800

    webdav

--- shell/docker/common/build_amd64_platform.sh
+++ shell/docker/common/build_amd64_platform.sh
@@ -8,10 +8,7 @@ docker push ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64
 docker manifest create ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64
 docker manifest push ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_AMD64 . --load
-docker push registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_AMD64
-docker manifest create registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME:$IMG_VERSION registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_AMD64
-docker manifest push registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME:$IMG_VERSION
+curl -fsSL https://www.qics.top/shell/docker/common/img_amd64_sync.sh | /bin/sh -s "ghcr.io/qicongsheng" "registry.cn-hangzhou.aliyuncs.com/qics" $IMG_NAME $IMG_VERSION
 
 docker rmi ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64
 docker rmi registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_AMD64
--- shell/docker/common/build_cross_platform.sh
+++ shell/docker/common/build_cross_platform.sh
@@ -11,12 +11,7 @@ docker push ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_ARM64
 docker manifest create ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64 ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_ARM64
 docker manifest push ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION
 
-docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_AMD64 --platform linux/amd64 . --load
-docker buildx build -t registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_ARM64 --platform linux/arm64 . --load
-docker push registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_AMD64
-docker push registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_ARM64
-docker manifest create registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME:$IMG_VERSION registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_AMD64 registry.cn-hangzhou.aliyuncs.com/qics/$IMG_FULL_NAME_PLATFORM_ARM64
-docker manifest push registry.cn-hangzhou.aliyuncs.com/qics/$IMG_NAME:$IMG_VERSION
+curl -fsSL https://www.qics.top/shell/docker/common/img_cross_sync.sh | /bin/sh -s "ghcr.io/qicongsheng" "registry.cn-hangzhou.aliyuncs.com/qics" $IMG_NAME $IMG_VERSION
 
 docker rmi ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64
 docker rmi ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_ARM64
