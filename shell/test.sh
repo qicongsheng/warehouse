b141ae9545c18c5ed5a7979a438b187eae9b7b21
qicongsheng <875881559@qq.com>
2024-06-14 14:23:12 +0800

    webdav

--- shell/test.sh
+++ shell/test.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-curl -fsSL "https://www.qics.top/shell/docker/common/docker_login.sh" | /bin/sh
-#添加onedriver
-curl -fsSL "https://www.qics.top/shell/rclone/rclone-debian-amd64.sh" | /bin/sh
-ls /onedriver
-cp /onedriver/oracle12c.tar /tmp
-cd /tmp
-docker load < oracle12c.tar
-
-
-
+curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
+docker pull ghcr.io/qicongsheng/oracle12c:latest
+docker tag ghcr.io/qicongsheng/oracle12c:latest ghcr.io/qicongsheng/oracle12c:latest_amd64
+docker push ghcr.io/qicongsheng/oracle12c:latest_amd64
+docker manifest create ghcr.io/qicongsheng/oracle12c:latest ghcr.io/qicongsheng/oracle12c:latest_amd64
+docker manifest push ghcr.io/qicongsheng/oracle12c:latest
