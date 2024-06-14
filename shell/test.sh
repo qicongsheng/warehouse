a846d8a53a302338fc66b602a35f12fbdab6bc8a
qicongsheng <875881559@qq.com>
2024-06-14 14:38:50 +0800

    webdav

--- shell/test.sh
+++ shell/test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
-docker pull ghcr.io/qicongsheng/oracle12c:latest
-docker tag ghcr.io/qicongsheng/oracle12c:latest ghcr.io/qicongsheng/oracle12c:latest_amd64
-docker push ghcr.io/qicongsheng/oracle12c:latest_amd64
-docker manifest create ghcr.io/qicongsheng/oracle12c:latest ghcr.io/qicongsheng/oracle12c:latest_amd64
-docker manifest push ghcr.io/qicongsheng/oracle12c:latest
+REGISTRY_SOURCE="ghcr.io/qicongsheng"
+REGISTRY_TARGET="registry.cn-hangzhou.aliyuncs.com/qics"
+curl -fsSL https://www.qics.top/shell/docker/common/img_cross_sync.sh | /bin/sh -s $REGISTRY_SOURCE $REGISTRY_TARGET "frp"              "server"
