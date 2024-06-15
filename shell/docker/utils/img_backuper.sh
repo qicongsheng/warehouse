ef81ea217ca2b4eadfd30ae1041b31392931fafb
qicongsheng <875881559@qq.com>
2024-06-15 13:31:02 +0800

    webdav

--- shell/docker/utils/img_backuper.sh
+++ shell/docker/utils/img_backuper.sh
@@ -2,4 +2,7 @@
 # curl -fsSL https://www.qics.top/shell/docker/utils/img_backuper.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
-REGISTRY_SOURCE="ghcr.io/qicongsheng"
+REGISTRY_URI="registry.cn-hangzhou.aliyuncs.com/qics"
+
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "alist"            "latest"
+
