c8711c281d0c8a0be8ce880e2cd1487c3393a191
qicongsheng <875881559@qq.com>
2024-06-15 13:41:43 +0800

    webdav

--- shell/docker/utils/img_backuper.sh
+++ shell/docker/utils/img_backuper.sh
@@ -4,9 +4,6 @@ curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
 REGISTRY_URI="registry.cn-hangzhou.aliyuncs.com/qics"
 
 curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "alist"            "latest"
-
-
-curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "alist"            "latest"
 curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "alpine"           "latest"
 curl -fsSL https://www.qics.top/shell/docker/common/backup_amd64_platform.sh | /bin/sh -s $REGISTRY_URI "chrome"           "latest"
 curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "playdocker"       "latest"
