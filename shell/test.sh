43c6fc1ac178a5862b07aadd257c54e51b5e131a
qicongsheng <875881559@qq.com>
2024-06-16 00:06:07 +0800

    webdav

--- shell/test.sh
+++ shell/test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# curl -fsSL https://www.qics.top/shell/test.sh | /bin/sh
 curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
-REGISTRY_SOURCE="ghcr.io/qicongsheng"
-REGISTRY_TARGET="registry.cn-hangzhou.aliyuncs.com/qics"
-curl -fsSL https://www.qics.top/shell/docker/common/img_cross_sync.sh | /bin/sh -s $REGISTRY_SOURCE $REGISTRY_TARGET "frp"              "server"
+REGISTRY_URI="registry.cn-hangzhou.aliyuncs.com/qics"
+
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "debian"           "latest"
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "backup"           "huawei"
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "backup"           "lenovo"
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "backup"           "tencent"
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "frp"              "server"
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "frp"              "client"
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "sc"               "rabbitmq"
+curl -fsSL https://www.qics.top/shell/docker/common/backup_cross_platform.sh | /bin/sh -s $REGISTRY_URI "sc"               "redis"
