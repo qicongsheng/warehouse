09c2169a99dd2ff7091a30409a61369b27b10293
qicongsheng <875881559@qq.com>
2024-06-16 12:10:18 +0800

    webdav

--- shell/docker/common/build_amd64_platform.sh
+++ shell/docker/common/build_amd64_platform.sh
@@ -5,7 +5,7 @@ IMG_FULL_NAME_PLATFORM_AMD64="${IMG_NAME}:${IMG_VERSION}_amd64"
 
 docker build -t ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64 . --load
 docker push ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64
-docker manifest create ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64
+docker manifest create ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64 --amend
 docker manifest push ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION
 
 curl -fsSL https://www.qics.top/shell/docker/common/img_amd64_sync.sh | /bin/sh -s "ghcr.io/qicongsheng" "registry.cn-hangzhou.aliyuncs.com/qics" $IMG_NAME $IMG_VERSION
--- shell/docker/common/build_cross_platform.sh
+++ shell/docker/common/build_cross_platform.sh
@@ -8,7 +8,7 @@ docker buildx build -t ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64 --platf
 docker buildx build -t ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_ARM64 --platform linux/arm64 . --load
 docker push ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64
 docker push ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_ARM64
-docker manifest create ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64 ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_ARM64
+docker manifest create ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_AMD64 ghcr.io/qicongsheng/$IMG_FULL_NAME_PLATFORM_ARM64 --amend
 docker manifest push ghcr.io/qicongsheng/$IMG_NAME:$IMG_VERSION
 
 curl -fsSL https://www.qics.top/shell/docker/common/img_cross_sync.sh | /bin/sh -s "ghcr.io/qicongsheng" "registry.cn-hangzhou.aliyuncs.com/qics" $IMG_NAME $IMG_VERSION
