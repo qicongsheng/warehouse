adff3e3346fb285848fb46dc81d44450ae3c03a3
qicongsheng <875881559@qq.com>
2024-06-16 12:02:20 +0800

    webdav

--- shell/test.sh
+++ shell/test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # curl -fsSL https://www.qics.top/shell/test.sh | /bin/sh
-curl -fsSL https://www.qics.top/shell/docker/utils/img_syncer.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/docker_login.sh | /bin/sh
+curl -fsSL https://www.qics.top/shell/docker/common/buildx_init.sh | /bin/sh
+REGISTRY_SOURCE="registry.cn-hangzhou.aliyuncs.com/qics"
+REGISTRY_TARGET="ccr.ccs.tencentyun.com/qicongsheng"
+
+curl -fsSL https://www.qics.top/shell/docker/common/sync_cross_platform.sh | /bin/sh -s $REGISTRY_SOURCE $REGISTRY_TARGET "dind"             "latest"
